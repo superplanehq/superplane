2be632159877da25150fd80213f2a5f19ef670d1
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-05-28 11:11:17 -0300

    fix: lint

--- pkg/cli/utils.go
+++ pkg/cli/utils.go
@@ -14,14 +14,18 @@ func getOneOrAnotherFlag(cmd *cobra.Command, flag1, flag2 string) string {
 	if flag1Value != "" && flag2Value != "" {
 		fmt.Fprintf(os.Stderr, "Error: cannot specify both --%s and --%s\n", flag1, flag2)
 		os.Exit(1)
-	} else if flag1Value != "" {
-		return flag1Value
-	} else if flag2Value != "" {
-		return flag2Value
-	} else {
-		fmt.Fprintf(os.Stderr, "Error: must specify either --%s or --%s\n", flag1, flag2)
-		os.Exit(1)
 	}
+
+	if flag1Value != "" {
+		return flag1Value
+	}
+
+	if flag2Value != "" {
+		return flag2Value
+	}
+
+	fmt.Fprintf(os.Stderr, "Error: must specify either --%s or --%s\n", flag1, flag2)
+	os.Exit(1)
 
 	return ""
 }
