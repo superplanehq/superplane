ffc78ed5201f7c158941476796235dc36a215a63
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-06-05 17:12:36 -0300

    feat: implement role assignment with new policies

--- pkg/authorization/auth.go
+++ pkg/authorization/auth.go
@@ -1,9 +1,12 @@
 package authorization
 
 import (
+	"bytes"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"net/http"
-	"strings"
+	"os"
 
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
@@ -13,38 +16,26 @@ import (
 )
 
 const (
-	RoleOrgOwner  = "org:owner"
-	RoleOrgAdmin  = "org:admin"
-	RoleOrgMember = "org:member"
+	OrgIDTemplate    = "{ORG_ID}"
+	CanvasIDTemplate = "{CANVAS_ID}"
 
-	RoleCanvasOwner       = "canvas:owner"
-	RoleCanvasAdmin       = "canvas:admin"
-	RoleCanvasDeveloper   = "canvas:developer"
-	RoleCanvasContributor = "canvas:contributor"
-	RoleCanvasViewer      = "canvas:viewer"
+	RoleOrgOwner  = "org_owner"
+	RoleOrgAdmin  = "org_admin"
+	RoleOrgViewer = "org_viewer"
 
-	ActionRead   = "read"
-	ActionWrite  = "write"
-	ActionCreate = "create"
-	ActionDelete = "delete"
-	ActionAdmin  = "admin"
+	RoleCanvasOwner  = "canvas_owner"
+	RoleCanvasAdmin  = "canvas_admin"
+	RoleCanvasViewer = "canvas_viewer"
+
+	DomainCanvas = "canvas"
+	DomainOrg    = "org"
 )
-const (
-	ResourceOrganization = "organization"
-	ResourceCanvas       = "canvas"
-	ResourceStage        = "stage"
-	ResourceExecution    = "execution"
-	ResourceEventSource  = "event_source"
-	ResourceSecret       = "secret"
-	ResourceUser         = "user"
-)
-
-// make sure AuthService implements AuthorizationService
-var _ AuthorizationService = (*AuthService)(nil)
 
 type AuthService struct {
-	enforcer *casbin.Enforcer
-	db       *gorm.DB
+	enforcer              *casbin.CachedEnforcer
+	db                    *gorm.DB
+	orgPolicyTemplates    [][5]string
+	canvasPolicyTemplates [][5]string
 }
 
 func NewAuthService() (*AuthService, error) {
@@ -53,359 +44,230 @@ func NewAuthService() (*AuthService, error) {
 		return nil, fmt.Errorf("failed to create casbin adapter: %w", err)
 	}
 
-	enforcer, err := casbin.NewEnforcer("../config/rbac_model.conf", adapter)
+	enforcer, err := casbin.NewCachedEnforcer("../config/rbac_model.conf", adapter)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create casbin enforcer: %w", err)
 	}
+
 	enforcer.EnableAutoSave(true)
 
 	if err := enforcer.LoadPolicy(); err != nil {
 		return nil, fmt.Errorf("failed to load policies: %w", err)
 	}
 
-	service := &AuthService{
-		enforcer: enforcer,
-		db:       database.Conn(),
+	orgPoliciesCsv, err := os.ReadFile("../config/rbac_org_policy.csv")
+	if err != nil {
+		return nil, fmt.Errorf("failed to read org policies: %w", err)
+	}
+	canvasPoliciesCsv, err := os.ReadFile("../config/rbac_canvas_policy.csv")
+	if err != nil {
+		return nil, fmt.Errorf("failed to read canvas policies: %w", err)
 	}
 
-	if err := service.initializeDefaultPolicies(); err != nil {
-		log.Warnf("Failed to initialize default policies: %v", err)
+	orgPolicyTemplates, err := parsePoliciesFromCsv(orgPoliciesCsv)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse org policies: %w", err)
+	}
+
+	canvasPolicyTemplates, err := parsePoliciesFromCsv(canvasPoliciesCsv)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse canvas policies: %w", err)
+	}
+
+	service := &AuthService{
+		enforcer:              enforcer,
+		db:                    database.Conn(),
+		orgPolicyTemplates:    orgPolicyTemplates,
+		canvasPolicyTemplates: canvasPolicyTemplates,
 	}
 
 	return service, nil
 }
 
-func (a *AuthService) CheckPermission(userID, resource, action string) (bool, error) {
-	allowed, err := a.enforcer.Enforce(userID, resource, action)
+func (a *AuthService) CheckCanvasPermission(userID, canvasID, resource, action string) (bool, error) {
+	return a.CheckPermission(userID, canvasID, DomainCanvas, resource, action)
+}
+
+func (a *AuthService) CheckOrganizationPermission(userID, orgID, resource, action string) (bool, error) {
+	return a.CheckPermission(userID, orgID, DomainOrg, resource, action)
+}
+
+func (a *AuthService) CheckPermission(userID, domainID, domainType, resource, action string) (bool, error) {
+	domain := fmt.Sprintf("%s:%s", domainType, domainID)
+	return a.enforcer.Enforce(userID, domain, resource, action)
+}
+
+func (a *AuthService) CreateGroup(orgID string, groupName string, role string) error {
+	return nil
+}
+
+func (a *AuthService) AddUserToGroup(orgID string, userID string, group string) error {
+	return nil
+}
+
+func (a *AuthService) RemoveUserFromGroup(orgID string, userID string, group string) error {
+	return nil
+}
+
+func (a *AuthService) GetGroupUsers(orgID string, group string) ([]string, error) {
+	return nil, nil
+}
+
+func (a *AuthService) GetGroups(orgID string) ([]string, error) {
+	return nil, nil
+}
+
+func (a *AuthService) GetGroupRoles(orgID string, group string) ([]string, error) {
+	return nil, nil
+}
+
+func (a *AuthService) AssignRole(userID, role, domainID string, domainType string) error {
+	validRoles := map[string][]string{
+		DomainOrg:    {RoleOrgViewer, RoleOrgAdmin, RoleOrgOwner},
+		DomainCanvas: {RoleCanvasViewer, RoleCanvasAdmin, RoleCanvasOwner},
+	}
+
+	if roles, exists := validRoles[domainType]; exists {
+		if !contains(roles, role) {
+			return fmt.Errorf("invalid role %s for domain type %s", role, domainType)
+		}
+	}
+
+	ruleAdded, err := a.enforcer.AddGroupingPolicy(userID, role, fmt.Sprintf("%s:%s", domainType, domainID))
 	if err != nil {
-		return false, fmt.Errorf("permission check failed: %w", err)
-	}
-	return allowed, nil
-}
-
-func (a *AuthService) CheckCanvasPermission(userID, canvasID, action string) (bool, error) {
-	resource := fmt.Sprintf("canvas:%s", canvasID)
-	allowed, err := a.enforcer.Enforce(userID, resource, action)
-	if err != nil {
-		return false, fmt.Errorf("permission check failed: %w", err)
-	}
-	if allowed {
-		return true, nil
+		return fmt.Errorf("failed to add role: %w", err)
 	}
 
-	canvasRoles := []string{
-		RoleCanvasOwner,
-		RoleCanvasAdmin,
-		RoleCanvasDeveloper,
-		RoleCanvasContributor,
-		RoleCanvasViewer,
-	}
-
-	for _, role := range canvasRoles {
-		hasRole, err := a.enforcer.HasRoleForUser(userID, fmt.Sprintf("%s:%s", role, canvasID))
-		if err != nil {
-			continue
-		}
-		if hasRole {
-			roleAllowed, err := a.enforcer.Enforce(role, "canvas", action)
-			if err != nil {
-				continue
-			}
-			if roleAllowed {
-				return true, nil
-			}
-		}
-	}
-
-	return false, nil
-}
-
-func (a *AuthService) CheckOrganizationPermission(userID, orgID, action string) (bool, error) {
-	resource := fmt.Sprintf("organization:%s", orgID)
-	return a.CheckPermission(userID, resource, action)
-}
-
-func (a *AuthService) AssignRole(userID, role, resourceID string) error {
-	if resourceID == "" {
-		_, err := a.enforcer.AddRoleForUser(userID, role)
-		if err != nil {
-			return fmt.Errorf("failed to assign role: %w", err)
-		}
-	} else {
-		resourceRole := fmt.Sprintf("%s:%s", role, resourceID)
-		_, err := a.enforcer.AddRoleForUser(userID, resourceRole)
-		if err != nil {
-			return fmt.Errorf("failed to assign resource role: %w", err)
-		}
+	if !ruleAdded {
+		log.Infof("role %s already exists for user %s", role, userID)
 	}
 
 	return nil
 }
 
-func (a *AuthService) RemoveRole(userID, role, resourceID string) error {
-	if resourceID == "" {
-		_, err := a.enforcer.DeleteRoleForUser(userID, role)
-		if err != nil {
-			return fmt.Errorf("failed to remove role: %w", err)
-		}
-	} else {
-		resourceRole := fmt.Sprintf("%s:%s", role, resourceID)
-		_, err := a.enforcer.DeleteRoleForUser(userID, resourceRole)
-		if err != nil {
-			return fmt.Errorf("failed to remove resource role: %w", err)
-		}
+func (a *AuthService) RemoveRole(userID, role, domainID string, domainType string) error {
+	domain := fmt.Sprintf("%s:%s", domainType, domainID)
+	ruleRemoved, err := a.enforcer.RemoveGroupingPolicy(userID, role, domain)
+	if err != nil {
+		return fmt.Errorf("failed to remove role: %w", err)
 	}
-
+	if !ruleRemoved {
+		log.Infof("role %s not found for user %s", role, userID)
+	}
 	return nil
 }
 
 func (a *AuthService) GetUserRoles(userID string) ([]string, error) {
-	roles, err := a.enforcer.GetRolesForUser(userID)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get user roles: %w", err)
-	}
-	return roles, nil
+	return a.enforcer.GetImplicitRolesForUser(userID)
 }
 
 func (a *AuthService) GetUsersForRole(role string) ([]string, error) {
-	users, err := a.enforcer.GetUsersForRole(role)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get users for role: %w", err)
-	}
-	return users, nil
+	return a.enforcer.GetImplicitUsersForRole(role)
 }
 
 func (a *AuthService) GetCanvasUsers(canvasID string) (map[string][]string, error) {
-	canvasUsers := make(map[string][]string)
+	return nil, nil
+}
 
-	canvasRoles := []string{
-		RoleCanvasOwner,
-		RoleCanvasAdmin,
-		RoleCanvasDeveloper,
-		RoleCanvasContributor,
-		RoleCanvasViewer,
-	}
+func (a *AuthService) SetupOrganizationRoles(orgID string) error {
+	domain := fmt.Sprintf("org:%s", orgID)
 
-	for _, role := range canvasRoles {
-		resourceRole := fmt.Sprintf("%s:%s", role, canvasID)
-		users, err := a.enforcer.GetUsersForRole(resourceRole)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get users for canvas role %s: %w", role, err)
-		}
-		if len(users) > 0 {
-			canvasUsers[role] = users
+	for _, policy := range a.orgPolicyTemplates {
+		if policy[0] == "g" {
+			// g,lower_role,higher_role,org:{ORG_ID}
+			a.enforcer.AddGroupingPolicy(policy[1], policy[2], domain)
+		} else if policy[0] == "p" {
+			// p,role,org:{ORG_ID},resource,action
+			a.enforcer.AddPolicy(policy[1], domain, policy[3], policy[4])
+		} else {
+			return fmt.Errorf("unknown policy type: %s", policy[0])
 		}
 	}
 
-	return canvasUsers, nil
+	return nil
 }
 
-func (a *AuthService) AddPermission(role, resource, action string) error {
-	_, err := a.enforcer.AddPolicy(role, resource, action)
-	if err != nil {
-		return fmt.Errorf("failed to add permission: %w", err)
-	}
-	return err
-}
+func (a *AuthService) SetupCanvasRoles(canvasID string) error {
+	domain := fmt.Sprintf("canvas:%s", canvasID)
 
-func (a *AuthService) RemovePermission(role, resource, action string) error {
-	_, err := a.enforcer.RemovePolicy(role, resource, action)
-	if err != nil {
-		return fmt.Errorf("failed to remove permission: %w", err)
+	for _, policy := range a.canvasPolicyTemplates {
+		if policy[0] == "g" {
+			// g,lower_role,higher_role,canvas:{CANVAS_ID}
+			a.enforcer.AddGroupingPolicy(policy[1], policy[2], domain)
+		} else if policy[0] == "p" {
+			// p,role,canvas:{CANVAS_ID},resource,action
+			a.enforcer.AddPolicy(policy[1], domain, policy[3], policy[4])
+		} else {
+			return fmt.Errorf("unknown policy type: %s", policy[0])
+		}
 	}
-	return err
+
+	return nil
 }
 
 func (a *AuthService) CreateOrganizationOwner(userID, orgID string) error {
-	return a.AssignRole(userID, RoleOrgOwner, orgID)
+	return a.AssignRole(userID, RoleOrgOwner, orgID, DomainOrg)
 }
 
-func (a *AuthService) InviteUserToCanvas(userID, canvasID, role string) error {
-	validRoles := []string{
-		RoleCanvasOwner,
-		RoleCanvasAdmin,
-		RoleCanvasDeveloper,
-		RoleCanvasContributor,
-		RoleCanvasViewer,
-	}
-
-	roleValid := false
-	for _, validRole := range validRoles {
-		if role == validRole {
-			roleValid = true
-			break
-		}
-	}
-
-	if !roleValid {
-		return fmt.Errorf("invalid canvas role: %s", role)
-	}
-
-	return a.AssignRole(userID, role, canvasID)
-}
-
-func (a *AuthService) initializeDefaultPolicies() error {
-	hasAnyPolicy, err := a.enforcer.HasPolicy("org:owner", "organization", "admin")
-	if err != nil {
-		return fmt.Errorf("failed to check for policies: %w", err)
-	}
-
-	if hasAnyPolicy {
-		return nil // Skip initialization
-	}
-
-	orgPolicies := [][]string{
-		{RoleOrgOwner, ResourceOrganization, ActionAdmin},
-		{RoleOrgOwner, ResourceCanvas, ActionAdmin},
-		{RoleOrgOwner, ResourceUser, ActionAdmin},
-		{RoleOrgOwner, ResourceSecret, ActionAdmin},
-
-		{RoleOrgAdmin, ResourceCanvas, ActionAdmin},
-		{RoleOrgAdmin, ResourceUser, ActionWrite},
-		{RoleOrgAdmin, ResourceSecret, ActionWrite},
-		{RoleOrgAdmin, ResourceOrganization, ActionRead},
-
-		{RoleOrgMember, ResourceOrganization, ActionRead},
-		{RoleOrgMember, ResourceCanvas, ActionRead},
-	}
-
-	canvasPolicies := [][]string{
-		// Canvas Owner - should have ALL permissions
-		{RoleCanvasOwner, ResourceCanvas, ActionRead},
-		{RoleCanvasOwner, ResourceCanvas, ActionWrite},
-		{RoleCanvasOwner, ResourceCanvas, ActionCreate},
-		{RoleCanvasOwner, ResourceCanvas, ActionDelete},
-		{RoleCanvasOwner, ResourceCanvas, ActionAdmin},
-		{RoleCanvasOwner, ResourceStage, ActionAdmin},
-		{RoleCanvasOwner, ResourceExecution, ActionAdmin},
-		{RoleCanvasOwner, ResourceEventSource, ActionAdmin},
-
-		// Canvas Admin - inherits from developer + admin permissions
-		{RoleCanvasAdmin, ResourceCanvas, ActionRead},
-		{RoleCanvasAdmin, ResourceCanvas, ActionWrite},
-		{RoleCanvasAdmin, ResourceCanvas, ActionAdmin},
-		{RoleCanvasAdmin, ResourceStage, ActionWrite},
-		{RoleCanvasAdmin, ResourceExecution, ActionWrite},
-		{RoleCanvasAdmin, ResourceEventSource, ActionWrite},
-
-		// Canvas Developer - can write stages and create executions
-		{RoleCanvasDeveloper, ResourceCanvas, ActionRead},
-		{RoleCanvasDeveloper, ResourceCanvas, ActionWrite},
-		{RoleCanvasDeveloper, ResourceStage, ActionRead},
-		{RoleCanvasDeveloper, ResourceStage, ActionWrite},
-		{RoleCanvasDeveloper, ResourceExecution, ActionRead},
-		{RoleCanvasDeveloper, ResourceExecution, ActionCreate},
-		{RoleCanvasDeveloper, ResourceEventSource, ActionRead},
-
-		// Canvas Contributor - can create executions
-		{RoleCanvasContributor, ResourceCanvas, ActionRead},
-		{RoleCanvasContributor, ResourceStage, ActionRead},
-		{RoleCanvasContributor, ResourceExecution, ActionRead},
-		{RoleCanvasContributor, ResourceExecution, ActionCreate},
-		{RoleCanvasContributor, ResourceEventSource, ActionRead},
-
-		// Canvas Viewer - read only
-		{RoleCanvasViewer, ResourceCanvas, ActionRead},
-		{RoleCanvasViewer, ResourceStage, ActionRead},
-		{RoleCanvasViewer, ResourceExecution, ActionRead},
-		{RoleCanvasViewer, ResourceEventSource, ActionRead},
-	}
-
-	roleHierarchy := [][]string{
-		{RoleOrgOwner, RoleOrgAdmin},
-		{RoleOrgAdmin, RoleOrgMember},
-
-		{RoleCanvasOwner, RoleCanvasAdmin},
-		{RoleCanvasAdmin, RoleCanvasDeveloper},
-		{RoleCanvasDeveloper, RoleCanvasContributor},
-		{RoleCanvasContributor, RoleCanvasViewer},
-	}
-
-	for _, policy := range orgPolicies {
-		if _, err := a.enforcer.AddPolicy(policy[0], policy[1], policy[2]); err != nil {
-			log.Warnf("Failed to add policy %v: %v", policy, err)
-		}
-	}
-
-	for _, policy := range canvasPolicies {
-		if _, err := a.enforcer.AddPolicy(policy[0], policy[1], policy[2]); err != nil {
-			log.Warnf("Failed to add policy %v: %v", policy, err)
-		}
-	}
-
-	for _, inheritance := range roleHierarchy {
-		if _, err := a.enforcer.AddGroupingPolicy(inheritance[0], inheritance[1]); err != nil {
-			log.Warnf("Failed to add role inheritance %v: %v", inheritance, err)
-		}
-	}
-
-	return err
-}
-
-func (a *AuthService) GetEnforcer() *casbin.Enforcer {
+func (a *AuthService) GetEnforcer() *casbin.CachedEnforcer {
 	return a.enforcer
 }
 
 func (a *AuthService) Middleware() func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			userID := extractUserIDFromRequest(r)
-			if userID == "" {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
-				return
-			}
+			// userID := extractUserIDFromRequest(r)
+			// if userID == "" {
+			// 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			// 	return
+			// }
 
-			resource := determineResource(r.URL.Path)
-			action := mapHTTPMethodToAction(r.Method)
+			// resource := determineResource(r.URL.Path)
+			// action := mapHTTPMethodToAction(r.Method)
 
-			allowed, err := a.CheckPermission(userID, resource, action)
-			if err != nil {
-				log.Errorf("Authorization check failed: %v", err)
-				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-				return
-			}
+			// allowed, err := a.CheckPermission(userID, resource, action)
+			// if err != nil {
+			// 	log.Errorf("Authorization check failed: %v", err)
+			// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			// 	return
+			// }
 
-			if !allowed {
-				http.Error(w, "Forbidden", http.StatusForbidden)
-				return
-			}
+			// if !allowed {
+			// 	http.Error(w, "Forbidden", http.StatusForbidden)
+			// 	return
+			// }
 
-			next.ServeHTTP(w, r)
+			// next.ServeHTTP(w, r)
 		})
 	}
 }
 
-func extractUserIDFromRequest(r *http.Request) string {
-	return r.Header.Get("X-User-ID")
+func parsePoliciesFromCsv(content []byte) ([][5]string, error) {
+	var policies [][5]string
+
+	csvReader := csv.NewReader(bytes.NewReader(content))
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("error reading CSV: %v", err)
+		}
+		if len(record) != 5 {
+			return nil, fmt.Errorf("invalid CSV record: %v", record)
+		}
+		policies = append(policies, [5]string{record[0], record[1], record[2], record[3], record[4]})
+	}
+
+	return policies, nil
 }
 
-func determineResource(path string) string {
-	if strings.Contains(path, "/stage") {
-		return ResourceStage
+func contains(slice []string, item string) bool {
+	for _, s := range slice {
+		if s == item {
+			return true
+		}
 	}
-	if strings.Contains(path, "/execution") {
-		return ResourceExecution
-	}
-	if strings.Contains(path, "/sources") {
-		return ResourceEventSource
-	}
-	if strings.Contains(path, "/canvas") {
-		return ResourceCanvas
-	}
-	return "unknown"
-}
-
-func mapHTTPMethodToAction(method string) string {
-	switch method {
-	case "GET":
-		return ActionRead
-	case "POST":
-		return ActionCreate
-	case "PUT", "PATCH":
-		return ActionWrite
-	case "DELETE":
-		return ActionDelete
-	default:
-		return ActionRead
-	}
+	return false
 }
