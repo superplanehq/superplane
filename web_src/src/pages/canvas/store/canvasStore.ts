93a7bd2cd7430ff627d5c1bb0888225f92c6c531
Lucas Pinheiro <lpinheiro@semaphore.io>
2025-07-01 11:20:47 -0300

    fix: JS build failing due to unused function

--- web_src/src/pages/canvas/store/canvasStore.ts
+++ web_src/src/pages/canvas/store/canvasStore.ts
@@ -8,14 +8,6 @@ import { Connection, Viewport, applyNodeChanges, applyEdgeChanges } from '@xyflo
 import { AllNodeType, EdgeType } from '../types/flow';
 import { autoLayoutNodes, transformConnectionGroupsToNodes, transformEventSourcesToNodes, transformStagesToNodes, transformToEdges } from '../utils/flowTransformers';
 
-function generateFakeUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = Math.random() * 16 | 0;
-    const v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
 // Create the store
 export const useCanvasStore = create<CanvasState>((set, get) => ({
   // Initial state
