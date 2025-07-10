254e0767de81ff691c182feae6d42e91751bfe1a
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-07-10 02:35:28 -0300

    refactor: update approval requirement to support user,group or role

--- pkg/models/stage.go
+++ pkg/models/stage.go
@@ -20,6 +20,10 @@ const (
 
 	StageConditionTypeApproval   = "approval"
 	StageConditionTypeTimeWindow = "time-window"
+
+	ApprovalRequirementTypeUser  = "user"
+	ApprovalRequirementTypeRole  = "role"
+	ApprovalRequirementTypeGroup = "group"
 )
 
 type Stage struct {
@@ -187,7 +191,15 @@ func (c *TimeWindowCondition) inTimeWindow(now time.Time) bool {
 }
 
 type ApprovalCondition struct {
-	Count int `json:"count"`
+	Count        int                     `json:"count"`
+	RequiredFrom []ApprovalRequirement   `json:"required_from,omitempty"`
+}
+
+type ApprovalRequirement struct {
+	Type     string `json:"type"`     // "user", "role", or "group"
+	Name     string `json:"name"`     // username, role name, or group name
+	ID       string `json:"id"`       // user ID, role name, or group name (alternative to name)
+	Count    int    `json:"count"`    // number of approvals required from this requirement
 }
 
 type ExecutorSpec struct {
