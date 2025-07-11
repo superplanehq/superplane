89a5519d03794b9e5be49f12621ff38ce6f1ee7e
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-07-11 12:44:47 -0300

    fix: add default username for existing users

--- db/migrations/20250710170208_add-username-to-users.up.sql
+++ db/migrations/20250710170208_add-username-to-users.up.sql
@@ -1,10 +1,22 @@
 BEGIN;
 
--- Add username column with default value generated from name for existing users
+-- Add username column to users table
 ALTER TABLE users
-  ADD COLUMN username VARCHAR(255) UNIQUE 
-  DEFAULT (LOWER(REPLACE(name, ' ', '_')));
+  ADD COLUMN username VARCHAR(255) UNIQUE;
 
+-- Update users with username from GitHub account provider
+UPDATE users
+  SET username = ap.username
+  FROM account_providers ap
+  WHERE users.id = ap.user_id
+    AND ap.provider = 'github'
+    AND ap.username IS NOT NULL;
+
+-- Make username NOT NULL after populating existing records
+ALTER TABLE users
+  ALTER COLUMN username SET NOT NULL;
+
+-- Create index on username
 CREATE INDEX idx_users_username ON users(username);
 
 COMMIT;
