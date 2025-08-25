d44ee27ea5017121dfebb0f39554b0cda3a3fdc9
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-08-25 19:15:19 -0300

    feat: modularize cleanup service

--- pkg/workers/event_source_updated_consumer.go
+++ pkg/workers/event_source_updated_consumer.go
@@ -1,8 +1,6 @@
 package workers
 
 import (
-	"context"
-	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -10,20 +8,18 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/superplanehq/superplane/pkg/grpc/actions/messages"
 	"github.com/superplanehq/superplane/pkg/logging"
-	"github.com/superplanehq/superplane/pkg/models"
 	protos "github.com/superplanehq/superplane/pkg/protos/canvases"
 	"github.com/superplanehq/superplane/pkg/registry"
 	"google.golang.org/protobuf/proto"
-	"gorm.io/gorm"
 )
 
 const EventSourceUpdatedServiceName = "superplane" + "." + messages.DeliveryHubCanvasExchange + "." + messages.EventSourceUpdatedRoutingKey + ".worker-consumer"
 const EventSourceUpdatedConnectionName = "superplane"
 
 type EventSourceUpdatedConsumer struct {
-	Consumer    *tackle.Consumer
-	Registry    *registry.Registry
-	RabbitMQURL string
+	Consumer       *tackle.Consumer
+	CleanupService *ResourceCleanupService
+	RabbitMQURL    string
 }
 
 func NewEventSourceUpdatedConsumer(registry *registry.Registry, rabbitMQURL string) *EventSourceUpdatedConsumer {
@@ -35,9 +31,9 @@ func NewEventSourceUpdatedConsumer(registry *registry.Registry, rabbitMQURL stri
 	consumer.SetLogger(logger)
 
 	return &EventSourceUpdatedConsumer{
-		RabbitMQURL: rabbitMQURL,
-		Consumer:    consumer,
-		Registry:    registry,
+		RabbitMQURL:    rabbitMQURL,
+		Consumer:       consumer,
+		CleanupService: NewResourceCleanupService(registry),
 	}
 }
 
@@ -96,77 +92,5 @@ func (c *EventSourceUpdatedConsumer) Consume(delivery tackle.Delivery) error {
 	log.Infof("Processing resource change for event source %s: old=%s, new=%s",
 		eventSourceID, data.OldResourceId, data.NewResourceId)
 
-	oldResource, err := models.FindResourceByID(oldResourceID)
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			log.Warningf("Old resource %s not found, skipping cleanup", oldResourceID)
-			return nil
-		}
-		log.Errorf("Error finding old resource %s: %v", oldResourceID, err)
-		return err
-	}
-
-	logger := log.WithField("old_resource_id", oldResourceID)
-
-	externalEventSourceCount, err := models.CountExternalEventSourcesUsingResource(oldResourceID)
-	if err != nil {
-		logger.Errorf("Error counting external event sources using resource: %v", err)
-		return err
-	}
-
-	stagesCount, err := models.CountOtherStagesUsingResource(oldResourceID, uuid.Nil)
-	if err != nil {
-		logger.Errorf("Error counting internal event sources using resource: %v", err)
-		return err
-	}
-
-	totalUsages := externalEventSourceCount + stagesCount
-	if totalUsages > 0 {
-		logger.Infof("Resource is used by %d external event sources and %d stages, skipping cleanup",
-			externalEventSourceCount, stagesCount)
-		return nil
-	}
-
-	integration, err := models.FindIntegrationByID(oldResource.IntegrationID)
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			logger.Info("Integration not found, skipping cleanup")
-			return nil
-		}
-		logger.Errorf("Error finding integration: %v", err)
-		return err
-	}
-
-	resourceManager, err := c.Registry.NewResourceManager(context.Background(), integration)
-	if err != nil {
-		logger.Errorf("Error creating resource manager: %v", err)
-		return err
-	}
-
-	childResources, err := oldResource.FindChildren()
-	if err != nil {
-		logger.Errorf("Error finding child resources: %v", err)
-		return err
-	}
-
-	for _, childResource := range childResources {
-		logger.Infof("Cleaning up webhook resource %s", childResource.Id())
-		err = resourceManager.CleanupWebhook(oldResource, &childResource)
-		if err != nil {
-			logger.Errorf("Error cleaning up webhook: %v", err)
-		} else {
-			logger.Infof("Successfully cleaned up webhook %s", childResource.Id())
-		}
-	}
-
-	// Delete the old resource and its children from database since they're no longer used
-	err = models.DeleteResourceWithChildren(oldResourceID)
-	if err != nil {
-		logger.Errorf("Error deleting old resource and children from database: %v", err)
-		return err
-	}
-
-	logger.Infof("Successfully cleaned up and deleted old resource %s and %d child resources", oldResourceID, len(childResources))
-
-	return nil
+	return c.CleanupService.CleanupUnusedResource(oldResourceID, uuid.Nil)
 }
--- pkg/workers/stage_updated_consumer.go
+++ pkg/workers/stage_updated_consumer.go
@@ -1,8 +1,6 @@
 package workers
 
 import (
-	"context"
-	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -10,20 +8,18 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/superplanehq/superplane/pkg/grpc/actions/messages"
 	"github.com/superplanehq/superplane/pkg/logging"
-	"github.com/superplanehq/superplane/pkg/models"
 	protos "github.com/superplanehq/superplane/pkg/protos/canvases"
 	"github.com/superplanehq/superplane/pkg/registry"
 	"google.golang.org/protobuf/proto"
-	"gorm.io/gorm"
 )
 
 const StageUpdatedServiceName = "superplane" + "." + messages.DeliveryHubCanvasExchange + "." + messages.StageUpdatedRoutingKey + ".worker-consumer"
 const StageUpdatedConnectionName = "superplane"
 
 type StageUpdatedConsumer struct {
-	Consumer    *tackle.Consumer
-	Registry    *registry.Registry
-	RabbitMQURL string
+	Consumer       *tackle.Consumer
+	CleanupService *ResourceCleanupService
+	RabbitMQURL    string
 }
 
 func NewStageUpdatedConsumer(registry *registry.Registry, rabbitMQURL string) *StageUpdatedConsumer {
@@ -35,9 +31,9 @@ func NewStageUpdatedConsumer(registry *registry.Registry, rabbitMQURL string) *S
 	consumer.SetLogger(logger)
 
 	return &StageUpdatedConsumer{
-		RabbitMQURL: rabbitMQURL,
-		Consumer:    consumer,
-		Registry:    registry,
+		RabbitMQURL:    rabbitMQURL,
+		Consumer:       consumer,
+		CleanupService: NewResourceCleanupService(registry),
 	}
 }
 
@@ -96,77 +92,5 @@ func (c *StageUpdatedConsumer) Consume(delivery tackle.Delivery) error {
 	log.Infof("Processing resource change for stage %s: old=%s, new=%s",
 		stageID, data.OldResourceId, data.NewResourceId)
 
-	oldResource, err := models.FindResourceByID(oldResourceID)
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			log.Warningf("Old resource %s not found, skipping cleanup", oldResourceID)
-			return nil
-		}
-		log.Errorf("Error finding old resource %s: %v", oldResourceID, err)
-		return err
-	}
-
-	logger := log.WithField("old_resource_id", oldResourceID)
-
-	externalEventSourceCount, err := models.CountExternalEventSourcesUsingResource(oldResourceID)
-	if err != nil {
-		logger.Errorf("Error counting external event sources using resource: %v", err)
-		return err
-	}
-
-	otherStagesCount, err := models.CountOtherStagesUsingResource(oldResourceID, stageID)
-	if err != nil {
-		logger.Errorf("Error counting other stages using resource: %v", err)
-		return err
-	}
-
-	totalUsages := externalEventSourceCount + otherStagesCount
-	if totalUsages > 0 {
-		logger.Infof("Resource is used by %d external event sources and %d other stages, skipping cleanup",
-			externalEventSourceCount, otherStagesCount)
-		return nil
-	}
-
-	integration, err := models.FindIntegrationByID(oldResource.IntegrationID)
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			logger.Info("Integration not found, skipping cleanup")
-			return nil
-		}
-		logger.Errorf("Error finding integration: %v", err)
-		return err
-	}
-
-	resourceManager, err := c.Registry.NewResourceManager(context.Background(), integration)
-	if err != nil {
-		logger.Errorf("Error creating resource manager: %v", err)
-		return err
-	}
-
-	childResources, err := oldResource.FindChildren()
-	if err != nil {
-		logger.Errorf("Error finding child resources: %v", err)
-		return err
-	}
-
-	for _, childResource := range childResources {
-		logger.Infof("Cleaning up resource %s", childResource.Id())
-		err = resourceManager.CleanupWebhook(oldResource, &childResource)
-		if err != nil {
-			logger.Errorf("Error cleaning up webhook: %v", err)
-		} else {
-			logger.Infof("Successfully cleaned up webhook %s", childResource.Id())
-		}
-	}
-
-	// Delete the old resource and its children from database since they're no longer used
-	err = models.DeleteResourceWithChildren(oldResourceID)
-	if err != nil {
-		logger.Errorf("Error deleting old resource and children from database: %v", err)
-		return err
-	}
-
-	logger.Infof("Successfully cleaned up and deleted old resource %s and %d child resources", oldResourceID, len(childResources))
-
-	return nil
+	return c.CleanupService.CleanupUnusedResource(oldResourceID, stageID)
 }
