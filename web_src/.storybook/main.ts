58b3389b7dd17458121cfa061652a0905c588eef
Aleksandar Mitrovic <amitrovic@renderedtext.com>
2025-10-27 11:27:22 +0100

    Fix for Cloudflare build

--- web_src/.storybook/main.ts
+++ web_src/.storybook/main.ts
@@ -1,9 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import * as path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   "stories": [
