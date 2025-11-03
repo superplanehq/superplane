f16cb4705b1a76d81a3c90813945362f7408c620
Lucas Pinheiro <lpinheiro@semaphore.io>
2025-11-03 14:57:41 -0300

    chore: remove unused components from src/components/*

--- web_src/src/components/SettingsPage/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { CanvasMembers } from './CanvasMembers'
-export { CanvasSecrets } from './CanvasSecrets'
-export { CanvasIntegrations } from './CanvasIntegrations'
-export { CanvasDelete } from './CanvasDelete'
