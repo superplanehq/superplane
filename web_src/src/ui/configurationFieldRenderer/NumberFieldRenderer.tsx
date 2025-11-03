c5db42e951667bfc736716e3deef376a3f312717
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-11-03 15:45:22 -0300

    fix: number field renderer not setting a default value

--- web_src/src/ui/configurationFieldRenderer/NumberFieldRenderer.tsx
+++ web_src/src/ui/configurationFieldRenderer/NumberFieldRenderer.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Input } from '../input'
 import { FieldRendererProps } from './types'
 
 export const NumberFieldRenderer: React.FC<FieldRendererProps> = ({ field, value, onChange, hasError }) => {
   const numberOptions = field.typeOptions?.number
+
+  // Set initial value on first render if no value is present but there's a default
+  useEffect(() => {
+    if ((value === undefined || value === null) && field.defaultValue !== undefined) {
+      const defaultVal = Number(field.defaultValue)
+      if (!isNaN(defaultVal)) {
+        onChange(defaultVal)
+      }
+    }
+  }, [value, field.defaultValue, onChange])
 
   return (
     <Input
