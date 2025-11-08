d161b06007aac13b9c414b0f94784ad147cc636b
Igor Šarčević <igor@operately.com>
2025-11-08 22:38:32 +0100

    Add tests for execution timeout
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- pkg/components/merge/merge_test.go
+++ pkg/components/merge/merge_test.go
@@ -14,8 +14,8 @@ import (
 	"gorm.io/gorm"
 )
 
-func Test_Merge(t *testing.T) {
-	steps := NewMergeTestSteps(t)
+func Test_QueueProcessing(t *testing.T) {
+	steps := NewQueueProcessingSteps(t)
 
 	steps.CreateWorkflow()
 	steps.CreateEvents()
@@ -36,7 +36,22 @@ func Test_Merge(t *testing.T) {
 	steps.AssertQueueIsEmpty()
 }
 
-type MergeTestSteps struct {
+func Test_ExecutionTimeout(t *testing.T) {
+	steps := NewExecutionTimeoutSteps(t)
+
+	steps.CreateMergeNodeWithTimeout(2, "seconds")
+	steps.CreateNodeExecution()
+
+	m := &Merge{}
+
+	steps.ExecuteMergeNode(m)
+	steps.AssertTimeoutScheduled()
+
+	steps.SimulateTimeoutReached(m)
+	steps.AssertExecutionFinished()
+}
+
+type QueueProcessingSteps struct {
 	t  *testing.T
 	Tx *gorm.DB
 
@@ -50,13 +65,13 @@ type MergeTestSteps struct {
 	Process1Event *models.WorkflowEvent
 	Process2Event *models.WorkflowEvent
 
-	QueureItem1 *models.WorkflowNodeQueueItem
-	QueureItem2 *models.WorkflowNodeQueueItem
+	QueueItem1 *models.WorkflowNodeQueueItem
+	QueueItem2 *models.WorkflowNodeQueueItem
 }
 
-func NewMergeTestSteps(t *testing.T) *MergeTestSteps {
+func NewQueueProcessingSteps(t *testing.T) *QueueProcessingSteps {
 	require.NoError(t, database.TruncateTables())
-	return &MergeTestSteps{
+	return &QueueProcessingSteps{
 		Tx: database.Conn(),
 		t:  t,
 	}
@@ -69,7 +84,7 @@ func NewMergeTestSteps(t *testing.T) *MergeTestSteps {
 *           |           -> (merge)
 *           +--> (n2) /
  */
-func (s *MergeTestSteps) CreateWorkflow() {
+func (s *QueueProcessingSteps) CreateWorkflow() {
 	wf := &models.Workflow{ID: uuid.New()}
 	require.NoError(s.t, s.Tx.Create(wf).Error)
 
@@ -137,7 +152,7 @@ func (s *MergeTestSteps) CreateWorkflow() {
 	s.MergeNode = n4
 }
 
-func (s *MergeTestSteps) CreateEvents() {
+func (s *QueueProcessingSteps) CreateEvents() {
 	rootEvent := &models.WorkflowEvent{
 		WorkflowID: s.Wf.ID,
 		NodeID:     "start-node",
@@ -167,7 +182,7 @@ func (s *MergeTestSteps) CreateEvents() {
 	s.Process2Event = event2
 }
 
-func (s *MergeTestSteps) CreateQueueItems() {
+func (s *QueueProcessingSteps) CreateQueueItems() {
 	queueItem1 := &models.WorkflowNodeQueueItem{
 		WorkflowID:  s.Wf.ID,
 		NodeID:      s.ProcessNode1.NodeID,
@@ -184,56 +199,140 @@ func (s *MergeTestSteps) CreateQueueItems() {
 	}
 	require.NoError(s.t, s.Tx.Create(queueItem2).Error)
 
-	s.QueureItem1 = queueItem1
-	s.QueureItem2 = queueItem2
+	s.QueueItem1 = queueItem1
+	s.QueueItem2 = queueItem2
 }
 
-func (s *MergeTestSteps) ProcessFirstEvent(m *Merge) {
+func (s *QueueProcessingSteps) ProcessFirstEvent(m *Merge) {
 	fmt.Println("Processing first event")
 
-	ctx1, err := contexts.BuildProcessQueueContext(s.Tx, s.MergeNode, s.QueureItem1)
+	ctx1, err := contexts.BuildProcessQueueContext(s.Tx, s.MergeNode, s.QueueItem1)
 	assert.NoError(s.t, err)
 
 	err = m.ProcessQueueItem(*ctx1)
 	require.NoError(s.t, err)
 }
 
-func (s *MergeTestSteps) ProcessSecondEvent(m *Merge) {
+func (s *QueueProcessingSteps) ProcessSecondEvent(m *Merge) {
 	fmt.Println("Processing second event")
 
-	ctx2, err := contexts.BuildProcessQueueContext(s.Tx, s.MergeNode, s.QueureItem2)
+	ctx2, err := contexts.BuildProcessQueueContext(s.Tx, s.MergeNode, s.QueueItem2)
 	assert.NoError(s.t, err)
 
 	err = m.ProcessQueueItem(*ctx2)
 	require.NoError(s.t, err)
 }
 
-func (s *MergeTestSteps) AssertNodeExecutionCount(expectedCount int) {
+func (s *QueueProcessingSteps) AssertNodeExecutionCount(expectedCount int) {
 	var executions []models.WorkflowNodeExecution
 	require.NoError(s.t, s.Tx.Where("node_id = ?", s.MergeNode.NodeID).Find(&executions).Error)
 	assert.Equal(s.t, expectedCount, len(executions))
 }
 
-func (s *MergeTestSteps) AssertExecutionFinished() {
+func (s *QueueProcessingSteps) AssertExecutionFinished() {
 	var execution models.WorkflowNodeExecution
 	require.NoError(s.t, s.Tx.Where("node_id = ?", s.MergeNode.NodeID).First(&execution).Error)
 	assert.Equal(s.t, execution.State, models.WorkflowNodeExecutionStateFinished)
 }
 
-func (s *MergeTestSteps) AssertExecutionPending() {
+func (s *QueueProcessingSteps) AssertExecutionPending() {
 	var execution models.WorkflowNodeExecution
 	require.NoError(s.t, s.Tx.Where("node_id = ?", s.MergeNode.NodeID).First(&execution).Error)
 	assert.Equal(s.t, execution.State, models.WorkflowNodeExecutionStatePending)
 }
 
-func (s *MergeTestSteps) AssertQueueIsEmpty() {
+func (s *QueueProcessingSteps) AssertQueueIsEmpty() {
 	var count int64
 	require.NoError(s.t, s.Tx.Model(&models.WorkflowNodeQueueItem{}).Where("node_id = ?", s.MergeNode.NodeID).Count(&count).Error)
 	assert.Equal(s.t, int64(0), count)
 }
 
-func (s *MergeTestSteps) AssertNodeIsAllowedToProcessNextQueueItem() {
+func (s *QueueProcessingSteps) AssertNodeIsAllowedToProcessNextQueueItem() {
 	var node models.WorkflowNode
 	require.NoError(s.t, s.Tx.Where("node_id = ?", s.MergeNode.NodeID).First(&node).Error)
 	assert.Equal(s.t, models.WorkflowNodeStateReady, node.State)
+}
+
+type ExecutionTimeoutSteps struct {
+	t  *testing.T
+	Tx *gorm.DB
+
+	Wf        *models.Workflow
+	MergeNode *models.WorkflowNode
+
+	NodeExec *models.WorkflowNodeExecution
+}
+
+func NewExecutionTimeoutSteps(t *testing.T) *ExecutionTimeoutSteps {
+	require.NoError(t, database.TruncateTables())
+	return &ExecutionTimeoutSteps{
+		Tx: database.Conn(),
+		t:  t,
+	}
+}
+
+func (s *ExecutionTimeoutSteps) CreateMergeNodeWithTimeout(value int, unit string) {
+	wf := &models.Workflow{ID: uuid.New()}
+	require.NoError(s.t, s.Tx.Create(wf).Error)
+
+	mergeNode := &models.WorkflowNode{
+		WorkflowID: wf.ID,
+		NodeID:     "merge-node",
+		Type:       models.NodeTypeComponent,
+		Ref: datatypes.NewJSONType(models.NodeRef{Component: &models.ComponentRef{
+			Name: "merge",
+			Configuration: map[string]any{
+				"executionTimeout": map[string]any{
+					"value": value,
+					"unit":  unit,
+				},
+			},
+		}}),
+	}
+	require.NoError(s.t, s.Tx.Create(mergeNode).Error)
+
+	s.Wf = wf
+	s.MergeNode = mergeNode
+}
+
+func (s *ExecutionTimeoutSteps) CreateNodeExecution() {
+	nodeExec := &models.WorkflowNodeExecution{
+		WorkflowID: s.Wf.ID,
+		NodeID:     s.MergeNode.NodeID,
+		State:      models.WorkflowNodeExecutionStatePending,
+	}
+	require.NoError(s.t, s.Tx.Create(nodeExec).Error)
+
+	s.NodeExec = nodeExec
+}
+
+func (s *ExecutionTimeoutSteps) ExecuteMergeNode(m *Merge) {
+	ctx, err := contexts.BuildNodeExecutionContext(s.Tx, s.MergeNode, s.NodeExec)
+	assert.NoError(s.t, err)
+
+	err = m.Execute(*ctx)
+	require.NoError(s.t, err)
+}
+
+func (s *ExecutionTimeoutSteps) AssertTimeoutScheduled() {
+	var actionCalls []models.WorkflowNodeActionCall
+	require.NoError(s.t, s.Tx.Where("node_id = ?", s.MergeNode.NodeID).Find(&actionCalls).Error)
+
+	found := false
+	for _, ac := range actionCalls {
+		if ac.Name == "timeoutReached" {
+			found = true
+			break
+		}
+	}
+
+	assert.True(s.t, found, "Expected timeoutReached action to be scheduled")
+}
+
+func (s *ExecutionTimeoutSteps) SimulateTimeoutReached(m *Merge) {
+	ctx, err := contexts.BuildActionContext(s.Tx, s.MergeNode, s.NodeExec, "timeoutReached")
+	assert.NoError(s.t, err)
+
+	err = m.HandleAction(*ctx)
+	require.NoError(s.t, err)
 }
