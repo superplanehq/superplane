695a5b98942014dd69476f77b2cae834812772f2
Igor Šarčević <igor@operately.com>
2025-11-11 15:28:38 +0100

    feat: Wait and Timegate have a passthrough options
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- pkg/components/wait/wait.go
+++ pkg/components/wait/wait.go
@@ -116,12 +116,21 @@ func (w *Wait) Actions() []components.Action {
 		{
 			Name: "timeReached",
 		},
+		{
+			Name:           "passThrough",
+			Description:    "Push Through",
+			UserAccessible: true,
+		},
 	}
 }
 
 func (w *Wait) HandleAction(ctx components.ActionContext) error {
 	switch ctx.Name {
 	case "timeReached":
+		return ctx.ExecutionStateContext.Pass(map[string][]any{
+			components.DefaultOutputChannel.Name: {map[string]any{}},
+		})
+	case "passThrough":
 		return ctx.ExecutionStateContext.Pass(map[string][]any{
 			components.DefaultOutputChannel.Name: {map[string]any{}},
 		})
