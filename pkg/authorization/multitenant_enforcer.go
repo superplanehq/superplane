074dfb69e8cb45caa6404522830a73f1e07ee9b4
Igor Šarčević <igor@operately.com>
2025-11-17 18:16:27 +0100

    Org provisioner
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- pkg/authorization/multitenant_enforcer.go
+++ pkg/authorization/multitenant_enforcer.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
+	log "github.com/sirupsen/logrus"
 	"github.com/superplanehq/superplane/pkg/database"
 	"github.com/superplanehq/superplane/pkg/models"
 )
@@ -29,13 +30,115 @@ func Verify(userID, orgID, resource, action string) (bool, error) {
 	return allowed, nil
 }
 
+//
+// Provision sets up default roles and policies for a new organization.
+//
+// CLEAN ME UP!
+
 func Provision(orgID string) error {
-	authService, err := NewAuthService()
-	if err != nil {
-		return fmt.Errorf("failed to create auth service: %w", err)
+	if orgID == "" {
+		return fmt.Errorf("orgID cannot be empty")
 	}
 
-	return authService.Provision(orgID)
+	domain := prefixDomain(models.DomainTypeOrganization, orgID)
+
+	tx := database.Conn().Begin()
+
+	log.Infof("Setting up default organization role metadata for %s", orgID)
+
+	defaultRoles := []struct {
+		name        string
+		displayName string
+		description string
+	}{
+		{
+			name:        models.RoleOrgOwner,
+			displayName: models.DisplayNameOwner,
+			description: models.MetaDescOrgOwner,
+		},
+		{
+			name:        models.RoleOrgAdmin,
+			displayName: models.DisplayNameAdmin,
+			description: models.MetaDescOrgAdmin,
+		},
+		{
+			name:        models.RoleOrgViewer,
+			displayName: models.DisplayNameViewer,
+			description: models.MetaDescOrgViewer,
+		},
+	}
+
+	for _, role := range defaultRoles {
+		if err := models.UpsertRoleMetadataInTransaction(tx, role.name, models.DomainTypeOrganization, orgID, role.displayName, role.description); err != nil {
+			log.Errorf("Error setting up default organization role metadata for %s: %v", orgID, err)
+			tx.Rollback()
+			return fmt.Errorf("failed to upsert role metadata for %s: %w", role.name, err)
+		}
+	}
+
+	log.Infof("Role metadata added - adding policies for %s", orgID)
+
+	orgPolicyPath := os.Getenv("RBAC_ORG_POLICY_PATH")
+	orgPoliciesCsv, err := os.ReadFile(orgPolicyPath)
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to read org policies: %w", err)
+	}
+
+	orgPolicyTemplates, err := parsePoliciesFromCsv(orgPoliciesCsv)
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to parse org policies: %w", err)
+	}
+
+	enforcer, err := newMultiTenantEnforcer()
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to create multi-tenant enforcer: %w", err)
+	}
+
+	err = enforcer.WithOrganizationTransaction(context.Background(), orgID, func(casbinTx *casbin.Transaction) error {
+		for _, policy := range orgPolicyTemplates {
+			switch policy[0] {
+			case "g":
+				_, err := casbinTx.AddGroupingPolicy(policy[1], policy[2], domain)
+				if err != nil {
+					return fmt.Errorf("failed to add grouping policy: %w", err)
+				}
+			case "p":
+				_, err := casbinTx.AddPolicy(policy[1], domain, policy[3], policy[4])
+				if err != nil {
+					return fmt.Errorf("failed to add policy: %w", err)
+				}
+			default:
+				return fmt.Errorf("unknown policy type: %s", policy[0])
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Errorf("Error adding policies for %s: %v", orgID, err)
+		tx.Rollback()
+		return fmt.Errorf("failed to provision organization roles for %s: %w", orgID, err)
+	}
+
+	log.Infof("Policies added - loading policies for %s", orgID)
+
+	if err := enforcer.enforcer.LoadPolicy(); err != nil {
+		log.Errorf("Error loading policies after provisioning organization roles for %s: %v", orgID, err)
+		tx.Rollback()
+		return fmt.Errorf("failed to load policies after provisioning organization roles for %s: %w", orgID, err)
+	}
+
+	log.Infof("Policies loaded - committing transaction for %s", orgID)
+
+	tx.Commit()
+
+	log.Infof("Transaction committed for %s", orgID)
+
+	return nil
 }
 
 func Update(orgID string, fn func(*casbin.Transaction) error) error {
