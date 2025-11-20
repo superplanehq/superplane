dd18a97f71b5cdd4f152833f43a15eaab3d09d1b
Petar Perovic <perovic.petar@gmail.com>
2025-11-20 14:29:55 +0100

    fix: Correct class attribute to className in ViewToggle component

--- web_src/src/ui/ViewToggle/index.tsx
+++ web_src/src/ui/ViewToggle/index.tsx
@@ -11,7 +11,7 @@ namespace ViewToggle {
 function ViewToggle({ isCollapsed, onToggle, className = "" }: ViewToggle.Props) {
   return (
     <div className={`flex bg-white border border-border rounded-sm ${className}`}>
-      <div class="border-r border-border">
+      <div className="border-r border-border">
         <button
           onClick={onToggle}
           className={`flex items-center justify-center w-6 h-6 transition-colors ${
