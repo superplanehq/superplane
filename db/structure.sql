6e7c68e33c155fa6d329ea094a72587fe747871d
Pedro Foresti Leão <pedroforestileao@gmail.com>
2025-11-29 10:15:18 -0300

    update structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -98,7 +98,7 @@ CREATE TABLE public.blueprints (
 
 CREATE TABLE public.casbin_rule (
     id integer NOT NULL,
-    ptype character varying(100),
+    ptype character varying(100) NOT NULL,
     v0 character varying(100),
     v1 character varying(100),
     v2 character varying(100),
@@ -680,13 +680,6 @@ CREATE INDEX idx_blueprints_organization_id ON public.blueprints USING btree (or
 --
 
 CREATE UNIQUE INDEX idx_casbin_rule ON public.casbin_rule USING btree (ptype, v0, v1, v2, v3, v4, v5);
-
-
---
--- Name: idx_casbin_rule_ptype; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_casbin_rule_ptype ON public.casbin_rule USING btree (ptype);
 
 
 --
