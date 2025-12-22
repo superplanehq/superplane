738ee46e530c5f5f26f211e34b39544056d46ec0
Igor Šarčević <igor@operately.com>
2025-12-22 21:08:37 +0100

    Okta UI settings
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ web_src/src/hooks/useOktaSettings.ts
@@ -0,0 +1,59 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  organizationsGetOktaSettings,
+  organizationsUpdateOktaSettings,
+  organizationsRotateOktaScimToken,
+} from "../api-client/sdk.gen";
+import type {
+  OrganizationsGetOktaSettingsResponse,
+  OrganizationsUpdateOktaSettingsBody,
+  OrganizationsRotateOktaScimTokenBody,
+} from "../api-client/types.gen";
+
+export function useOktaSettings(organizationId: string) {
+  const queryClient = useQueryClient();
+
+  const query = useQuery<OrganizationsGetOktaSettingsResponse>({
+    queryKey: ["okta-settings", organizationId],
+    queryFn: async () => {
+      const res = await organizationsGetOktaSettings({
+        path: { id: organizationId },
+      });
+      return res.data;
+    },
+  });
+
+  const updateMutation = useMutation({
+    mutationFn: async (body: OrganizationsUpdateOktaSettingsBody) => {
+      return organizationsUpdateOktaSettings({
+        path: { id: organizationId },
+        body,
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["okta-settings", organizationId] });
+    },
+  });
+
+  const rotateTokenMutation = useMutation({
+    mutationFn: async (body: OrganizationsRotateOktaScimTokenBody) => {
+      const res = await organizationsRotateOktaScimToken({
+        path: { id: organizationId },
+        body,
+      });
+      return res.data;
+    },
+  });
+
+  return {
+    oktaSettings: query.data?.settings,
+    isLoading: query.isLoading,
+    error: query.error,
+    updateSettings: updateMutation.mutateAsync,
+    isUpdating: updateMutation.isPending,
+    rotateToken: rotateTokenMutation.mutateAsync,
+    isRotating: rotateTokenMutation.isPending,
+    rotatedToken: rotateTokenMutation.data?.token,
+  };
+}
+
