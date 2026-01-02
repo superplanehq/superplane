3123621e0bfaf05fcd074acd50882f7b2037b86c
Emil Todorovski <emil.todorovski@brightmarbles.io>
2026-01-02 14:50:09 +0100

    feat: github.onBranchCreated
    Signed-off-by: Emil Todorovski <emil.todorovski@brightmarbles.io>

---
+++ web_src/src/pages/workflowv2/mappers/github/on_branch_created.ts
@@ -0,0 +1,76 @@
+import { ComponentsNode, TriggersTrigger, WorkflowsWorkflowEvent } from "@/api-client";
+import { getColorClass, getBackgroundColorClass } from "@/utils/colors";
+import { TriggerRenderer } from "../types";
+import githubIcon from "@/assets/icons/integrations/github.svg";
+import { TriggerProps } from "@/ui/trigger";
+import { BaseNodeMetadata } from "./base";
+import { Predicate, createGithubMetadataItems } from "./utils";
+
+interface GithubConfiguration {
+  repository: string;
+  branches: Predicate[];
+}
+
+interface BranchCreatedEventData {
+  ref?: string;
+  ref_type?: string;
+  repository?: {
+    name?: string;
+    full_name?: string;
+  };
+  sender?: {
+    login?: string;
+  };
+}
+
+/**
+ * Renderer for the "github.onBranchCreated" trigger
+ */
+export const onBranchCreatedTriggerRenderer: TriggerRenderer = {
+  getTitleAndSubtitle: (event: WorkflowsWorkflowEvent): { title: string; subtitle: string } => {
+    const eventData = event.data?.data as BranchCreatedEventData;
+
+    return {
+      title: eventData?.ref ? `Branch: ${eventData.ref}` : "Branch Created",
+      subtitle: eventData?.repository?.full_name || "",
+    };
+  },
+
+  getRootEventValues: (lastEvent: WorkflowsWorkflowEvent): Record<string, string> => {
+    const eventData = lastEvent.data?.data as BranchCreatedEventData;
+
+    return {
+      Branch: eventData?.ref || "",
+      Repository: eventData?.repository?.full_name || "",
+      Sender: eventData?.sender?.login || "",
+    };
+  },
+
+  getTriggerProps: (node: ComponentsNode, trigger: TriggersTrigger, lastEvent: WorkflowsWorkflowEvent) => {
+    const metadata = node.metadata as unknown as BaseNodeMetadata;
+    const configuration = node.configuration as unknown as GithubConfiguration;
+
+    const props: TriggerProps = {
+      title: node.name!,
+      iconSrc: githubIcon,
+      iconBackground: "bg-white",
+      iconColor: getColorClass(trigger.color),
+      headerColor: getBackgroundColorClass(trigger.color),
+      collapsedBackground: getBackgroundColorClass(trigger.color),
+      metadata: createGithubMetadataItems(metadata?.repository?.name, configuration?.branches),
+    };
+
+    if (lastEvent) {
+      const eventData = lastEvent.data?.data as BranchCreatedEventData;
+      props.lastEventData = {
+        title: eventData?.ref ? `Branch: ${eventData.ref}` : "Branch Created",
+        subtitle: eventData?.repository?.full_name || "",
+        receivedAt: new Date(lastEvent.createdAt!),
+        state: "triggered",
+        eventId: lastEvent.id,
+      };
+    }
+
+    return props;
+  },
+};
