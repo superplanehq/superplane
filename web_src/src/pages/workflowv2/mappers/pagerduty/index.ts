7e67089c7fad7b0bc1bc39d7d91fd5292b50c2d5
Emil Todorovski <emil.todorovski@brightmarbles.io>
2026-01-15 18:34:31 +0100

    feat: PagerDuty Update Incident
    Signed-off-by: Emil Todorovski <emil.todorovski@brightmarbles.io>

--- web_src/src/pages/workflowv2/mappers/pagerduty/index.ts
+++ web_src/src/pages/workflowv2/mappers/pagerduty/index.ts
@@ -1,9 +1,11 @@
 import { ComponentBaseMapper, EventStateRegistry, TriggerRenderer } from "../types";
 import { onIncidentTriggerRenderer } from "./on_incident";
 import { createIncidentMapper } from "./create_incident";
+import { updateIncidentMapper } from "./update_incident";
 
 export const componentMappers: Record<string, ComponentBaseMapper> = {
   createIncident: createIncidentMapper,
+  updateIncident: updateIncidentMapper,
 };
 
 export const triggerRenderers: Record<string, TriggerRenderer> = {
