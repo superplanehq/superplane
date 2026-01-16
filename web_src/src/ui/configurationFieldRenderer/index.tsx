84a8baad86620d1f1b02ef9a8953aa7b0a801fae
Aleksandar Mitrovic <amitrovic@renderedtext.com>
2026-01-16 09:23:18 +0200

    Editing details tab content

--- web_src/src/ui/configurationFieldRenderer/index.tsx
+++ web_src/src/ui/configurationFieldRenderer/index.tsx
@@ -470,12 +470,6 @@ export const ConfigurationFieldRenderer = ({
         </p>
       )}
 
-      {/* Display type-specific help text */}
-      {field.typeOptions?.number?.min !== undefined && field.typeOptions?.number?.max !== undefined && (
-        <p className="text-xs text-gray-500 dark:text-gray-400 text-left">
-          Range: {field.typeOptions.number.min} - {field.typeOptions.number.max}
-        </p>
-      )}
     </div>
   );
 };
