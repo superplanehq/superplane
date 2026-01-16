858561408e2e24148d3165d906a0750f5e3d166a
Lucas Pinheiro <lucas@superplane.com>
2026-01-16 10:09:25 +0200

    fix: remove old integrations page (#1667)
    Signed-off-by: Lucas Pinheiro <lucas@superplane.com>

--- web_src/src/components/IntegrationForm/index.ts
+++
@@ -1,6 +0,0 @@
-export { GitHubIntegrationForm } from "./GitHubIntegrationForm";
-export { SemaphoreIntegrationForm } from "./SemaphoreIntegrationForm";
-export { ApiTokenForm } from "./ApiTokenForm";
-export { useIntegrationForm, NEW_SECRET_NAME } from "./useIntegrationForm";
-export { getIntegrationConfig } from "./integrationConfigs";
-export type { IntegrationData, FormErrors, BaseIntegrationFormProps, IntegrationConfig } from "./types";
