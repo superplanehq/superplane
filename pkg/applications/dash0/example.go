f7709f4e0f9c163b55b5b0f6aea091233751e0af
Pedro Foresti Leão <pedroforestileao@gmail.com>
2026-01-19 14:30:15 +0200

    fix: add example for list_issues
    Signed-off-by: Pedro Foresti Leão <pedroforestileao@gmail.com>

--- pkg/applications/dash0/example.go
+++ pkg/applications/dash0/example.go
@@ -13,6 +13,16 @@ var exampleOutputQueryPrometheusBytes []byte
 var exampleOutputQueryPrometheusOnce sync.Once
 var exampleOutputQueryPrometheus map[string]any
 
+//go:embed example_output_list_issues.json
+var exampleOutputListIssuesBytes []byte
+
+var exampleOutputListIssuesOnce sync.Once
+var exampleOutputListIssues map[string]any
+
 func (c *QueryPrometheus) ExampleOutput() map[string]any {
 	return utils.UnmarshalEmbeddedJSON(&exampleOutputQueryPrometheusOnce, exampleOutputQueryPrometheusBytes, &exampleOutputQueryPrometheus)
+}
+
+func (c *ListIssues) ExampleOutput() map[string]any {
+	return utils.UnmarshalEmbeddedJSON(&exampleOutputListIssuesOnce, exampleOutputListIssuesBytes, &exampleOutputListIssues)
 }
