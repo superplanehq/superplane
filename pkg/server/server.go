ce9bfe1074efd31155a313c3f6a395a4687680a2
Emil Todorovski <emil.todorovski@brightmarbles.io>
2026-01-20 12:42:02 +0100

    feat: discord integration with send text message
    Signed-off-by: Emil Todorovski <emil.todorovski@brightmarbles.io>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -21,6 +21,7 @@ import (
 
 	// Import integrations, components and triggers to register them via init()
 	_ "github.com/superplanehq/superplane/pkg/applications/dash0"
+	_ "github.com/superplanehq/superplane/pkg/applications/discord"
 	_ "github.com/superplanehq/superplane/pkg/applications/github"
 	_ "github.com/superplanehq/superplane/pkg/applications/openai"
 	_ "github.com/superplanehq/superplane/pkg/applications/pagerduty"
