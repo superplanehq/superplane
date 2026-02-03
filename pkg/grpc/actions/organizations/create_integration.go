9a0654a51111181f900134c6d23c8357456e138d
Lucas Pinheiro <lucas@superplane.com>
2026-02-03 17:11:21 -0300

    chore: Remove InstallationID from contexts (#2838)
    Components should use ctx.Integration.ID() instead.
    
    ---------
    
    Signed-off-by: Lucas Pinheiro <lucas@superplane.com>

--- pkg/grpc/actions/organizations/create_integration.go
+++ pkg/grpc/actions/organizations/create_integration.go
@@ -69,7 +69,6 @@ func CreateIntegration(ctx context.Context, registry *registry.Registry, oidcPro
 		BaseURL:         baseURL,
 		WebhooksBaseURL: webhooksBaseURL,
 		OrganizationID:  orgID,
-		InstallationID:  newIntegration.ID.String(),
 		OIDC:            oidcProvider,
 	})
 
