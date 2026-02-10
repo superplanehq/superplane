16dd7185b59c229f3f076b7bc4abeeb0aa971db2
Fadhili Juma <ffumwa@gmail.com>
2026-02-11 02:30:52 +0300

    feat: add Dash0 Update Synthetic Check action
    Signed-off-by: Fadhili Juma <ffumwa@gmail.com>

--- pkg/integrations/dash0/types.go
+++ pkg/integrations/dash0/types.go
@@ -88,3 +88,10 @@ type UpsertSyntheticCheckConfiguration struct {
 	OriginOrID string `json:"originOrId" mapstructure:"originOrId"`
 	Spec       string `json:"spec" mapstructure:"spec"`
 }
+
+// SyntheticCheck is a normalized synthetic check descriptor for resource listings.
+type SyntheticCheck struct {
+	ID     string `json:"id,omitempty"`
+	Name   string `json:"name,omitempty"`
+	Origin string `json:"origin,omitempty"`
+}
