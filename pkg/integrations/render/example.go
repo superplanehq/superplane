6e8995fda22a1212f045f2b9eb715f025a914d76
vikramships <zenohkai@gmail.com>
2026-02-10 23:20:42 +0530

    feat: Add Render Purge Cache and Update Env Var components
    Closes #2989 (3/3)
    
    Adds two service management components to the Render integration:
    - Purge Cache: clear build cache for a service
    - Update Env Var: update environment variables (set/generate strategies)
    
    Signed-off-by: vikramships <zenohkai@gmail.com>

--- pkg/integrations/render/example.go
+++ pkg/integrations/render/example.go
@@ -28,6 +28,12 @@ var exampleOutputCancelDeployBytes []byte
 //go:embed example_output_rollback_deploy.json
 var exampleOutputRollbackDeployBytes []byte
 
+//go:embed example_output_purge_cache.json
+var exampleOutputPurgeCacheBytes []byte
+
+//go:embed example_output_update_env_var.json
+var exampleOutputUpdateEnvVarBytes []byte
+
 var exampleDataOnDeployOnce sync.Once
 var exampleDataOnDeploy map[string]any
 
@@ -48,6 +54,12 @@ var exampleOutputCancelDeploy map[string]any
 
 var exampleOutputRollbackDeployOnce sync.Once
 var exampleOutputRollbackDeploy map[string]any
+
+var exampleOutputPurgeCacheOnce sync.Once
+var exampleOutputPurgeCache map[string]any
+
+var exampleOutputUpdateEnvVarOnce sync.Once
+var exampleOutputUpdateEnvVar map[string]any
 
 func (t *OnDeploy) ExampleData() map[string]any {
 	return utils.UnmarshalEmbeddedJSON(
@@ -102,5 +114,21 @@ func (c *RollbackDeploy) ExampleOutput() map[string]any {
 		&exampleOutputRollbackDeployOnce,
 		exampleOutputRollbackDeployBytes,
 		&exampleOutputRollbackDeploy,
+	)
+}
+
+func (c *PurgeCache) ExampleOutput() map[string]any {
+	return utils.UnmarshalEmbeddedJSON(
+		&exampleOutputPurgeCacheOnce,
+		exampleOutputPurgeCacheBytes,
+		&exampleOutputPurgeCache,
+	)
+}
+
+func (c *UpdateEnvVar) ExampleOutput() map[string]any {
+	return utils.UnmarshalEmbeddedJSON(
+		&exampleOutputUpdateEnvVarOnce,
+		exampleOutputUpdateEnvVarBytes,
+		&exampleOutputUpdateEnvVar,
 	)
 }
