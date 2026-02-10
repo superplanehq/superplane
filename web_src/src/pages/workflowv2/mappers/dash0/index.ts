14149007d8107b4edc11cabc11675d628cd36d24
Fadhili Juma <ffumwa@gmail.com>
2026-02-11 02:23:34 +0300

    feat: add Dash0 Create Synthetic Check action
    Signed-off-by: Fadhili Juma <ffumwa@gmail.com>

--- web_src/src/pages/workflowv2/mappers/dash0/index.ts
+++ web_src/src/pages/workflowv2/mappers/dash0/index.ts
@@ -6,12 +6,14 @@ import { listIssuesMapper, LIST_ISSUES_STATE_REGISTRY } from "./list_issues";
 import { buildActionStateRegistry } from "../utils";
 import { sendLogEventMapper } from "./send_log_event";
 import { getCheckDetailsMapper } from "./get_check_details";
+import { createSyntheticCheckMapper } from "./create_synthetic_check";
 
 export const componentMappers: Record<string, ComponentBaseMapper> = {
   queryPrometheus: queryPrometheusMapper,
   listIssues: listIssuesMapper,
   sendLogEvent: sendLogEventMapper,
   getCheckDetails: getCheckDetailsMapper,
+  createSyntheticCheck: createSyntheticCheckMapper,
 };
 
 export const triggerRenderers: Record<string, TriggerRenderer> = {};
@@ -21,6 +23,7 @@ export const eventStateRegistry: Record<string, EventStateRegistry> = {
   queryPrometheus: buildActionStateRegistry("queried"),
   sendLogEvent: buildActionStateRegistry("sent"),
   getCheckDetails: buildActionStateRegistry("retrieved"),
+  createSyntheticCheck: buildActionStateRegistry("created"),
 };
 
 export async function resolveExecutionErrors(canvasId: string, executionIds: string[]) {
