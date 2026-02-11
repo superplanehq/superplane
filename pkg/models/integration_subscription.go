ef068caf77a7098cfcdeffd11cd5c78785e47a95
Akinniranye Samuel Tomiwa <benneu40@gmail.com>
2026-02-10 23:35:57 -0800

    improve validation, query performance, and fix URL duplication
    Signed-off-by: samuel-asleep <benneu40@gmail.com>

--- pkg/models/integration_subscription.go
+++ pkg/models/integration_subscription.go
@@ -47,7 +47,14 @@ func CreateIntegrationSubscriptionInTransaction(tx *gorm.DB, node *CanvasNode, i
 }
 
 func DeleteIntegrationSubscription(tx *gorm.DB, id uuid.UUID) error {
-	return tx.Where("id = ?", id).Delete(&IntegrationSubscription{}).Error
+	result := tx.Where("id = ?", id).Delete(&IntegrationSubscription{})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
+	return nil
 }
 
 func DeleteIntegrationSubscriptionsForNodeInTransaction(tx *gorm.DB, workflowID uuid.UUID, nodeID string) error {
@@ -82,4 +89,25 @@ func ListIntegrationSubscriptions(tx *gorm.DB, installationID uuid.UUID) ([]Node
 	}
 
 	return subscriptions, nil
+}
+
+// FindIntegrationSubscriptionByMessageTS finds a subscription by installation_id and message_ts
+// without loading node information. This is useful for webhook handlers that only need
+// the subscription configuration.
+// Note: This function specifically filters for subscriptions with type='button_click'.
+func FindIntegrationSubscriptionByMessageTS(tx *gorm.DB, installationID uuid.UUID, messageTS string) (*IntegrationSubscription, error) {
+	var subscription IntegrationSubscription
+
+	err := tx.
+		Where("installation_id = ?", installationID).
+		Where("configuration->>'message_ts' = ?", messageTS).
+		Where("configuration->>'type' = ?", "button_click").
+		First(&subscription).
+		Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &subscription, nil
 }
