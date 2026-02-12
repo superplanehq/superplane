742d74bfd4e739dc33f18569fd16e7c37d83d9c7
Lucas Pinheiro <lucas@superplane.com>
2026-02-12 12:54:21 -0300

    feat: aws.cloudwatch.onAlarm trigger (#3023)
    Bootstrapping the AWS CloudWatch integration, adding a
    `aws.cloudwatch.onAlarm` trigger to receive events when AWS CloudWatch
    alarms transitions to a different state.
    
    ---------
    
    Signed-off-by: Lucas Pinheiro <lucas@superplane.com>
    
    Signed-off-by: Muhammad Fuzail Zubari <mfuzail.zubari@gmail.com>

--- web_src/src/ui/componentSidebar/integrationIcons.tsx
+++ web_src/src/ui/componentSidebar/integrationIcons.tsx
@@ -2,6 +2,7 @@ import { resolveIcon } from "@/lib/utils";
 import React from "react";
 import awsIcon from "@/assets/icons/integrations/aws.svg";
 import awsLambdaIcon from "@/assets/icons/integrations/aws.lambda.svg";
+import circleciIcon from "@/assets/icons/integrations/circleci.svg";
 import cloudflareIcon from "@/assets/icons/integrations/cloudflare.svg";
 import dash0Icon from "@/assets/icons/integrations/dash0.svg";
 import datadogIcon from "@/assets/icons/integrations/datadog.svg";
