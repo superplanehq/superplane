32bbd092f8d5b9c5b62b5b13b5f496e168f3dd9f
manasa <manasa.bhagwat.work@gmail.com>
2026-02-19 02:07:02 +0530

    feat: Add AWS CodePipeline Run Pipeline action
    Signed-off-by: manasa <manasa.bhagwat.work@gmail.com>

--- pkg/integrations/aws/resources.go
+++ pkg/integrations/aws/resources.go
@@ -3,6 +3,7 @@ package aws
 import (
 	"github.com/superplanehq/superplane/pkg/core"
 	"github.com/superplanehq/superplane/pkg/integrations/aws/codeartifact"
+	"github.com/superplanehq/superplane/pkg/integrations/aws/codepipeline"
 	"github.com/superplanehq/superplane/pkg/integrations/aws/ecr"
 	"github.com/superplanehq/superplane/pkg/integrations/aws/lambda"
 	"github.com/superplanehq/superplane/pkg/integrations/aws/sns"
@@ -21,6 +22,9 @@ func (a *AWS) ListResources(resourceType string, ctx core.ListResourcesContext) 
 
 	case "codeartifact.domain":
 		return codeartifact.ListDomains(ctx, resourceType)
+
+	case "codepipeline.pipeline":
+		return codepipeline.ListPipelines(ctx, resourceType)
 
 	case "sns.topic":
 		return sns.ListTopics(ctx, resourceType)
