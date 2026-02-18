4d19282e709811b96aca943a9dc503969063b5c0
Muhammad Fuzail Zubari <mfuzail.zubari@gmail.com>
2026-02-18 12:51:37 +0500

    fix(grafana): harden webhook secret lifecycle, preserve sharedSecret on merge, and fix mapper tsx build (#1945)
    Signed-off-by: Muhammad Fuzail Zubari <mfuzail.zubari@gmail.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -42,6 +42,7 @@ import (
 	_ "github.com/superplanehq/superplane/pkg/integrations/daytona"
 	_ "github.com/superplanehq/superplane/pkg/integrations/discord"
 	_ "github.com/superplanehq/superplane/pkg/integrations/dockerhub"
+	_ "github.com/superplanehq/superplane/pkg/integrations/grafana"
 	_ "github.com/superplanehq/superplane/pkg/integrations/github"
 	_ "github.com/superplanehq/superplane/pkg/integrations/gitlab"
 	_ "github.com/superplanehq/superplane/pkg/integrations/hetzner"
