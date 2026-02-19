fccfba96e6e3fb05ea744f29ef8a479b4ecc7976
Pedro F. Leao <pedroforestileao@gmail.com>
2026-02-19 19:17:04 -0300

    chore: add _targer blank and noreferrer to markdown renderer
    Signed-off-by: Pedro F. Leao <pedroforestileao@gmail.com>

--- web_src/src/ui/IntegrationInstructions.tsx
+++ web_src/src/ui/IntegrationInstructions.tsx
@@ -39,7 +39,12 @@ export function IntegrationInstructions({ description, onContinue, className = "
               ol: ({ children }) => <ol className="list-decimal ml-5 space-y-1 mb-2">{children}</ol>,
               li: ({ children }) => <li>{children}</li>,
               a: ({ children, href }) => (
-                <a className="!underline underline-offset-2 decoration-2 decoration-current" href={href}>
+                <a
+                  className="!underline underline-offset-2 decoration-2 decoration-current"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={href}
+                >
                   {children}
                 </a>
               ),
