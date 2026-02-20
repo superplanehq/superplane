e0fec284ac9b44f8ddef3eb7f603501786e16e66
Milos Jovanovic <milosjovanovic519@gmail.com>
2026-02-20 02:20:04 +0000

    fix: apply gofmt to server imports
    Signed-off-by: Milos Jovanovic <milosjovanovic519@gmail.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -48,7 +48,7 @@ import (
 	_ "github.com/superplanehq/superplane/pkg/integrations/gitlab"
 	_ "github.com/superplanehq/superplane/pkg/integrations/grafana"
 	_ "github.com/superplanehq/superplane/pkg/integrations/harness"
-  _ "github.com/superplanehq/superplane/pkg/integrations/hetzner"
+	_ "github.com/superplanehq/superplane/pkg/integrations/hetzner"
 	_ "github.com/superplanehq/superplane/pkg/integrations/jira"
 	_ "github.com/superplanehq/superplane/pkg/integrations/openai"
 	_ "github.com/superplanehq/superplane/pkg/integrations/pagerduty"
