344b08fb8db8a6215cac10463389905d36b6ff88
Jilks Smith <smithjilks@gmail.com>
2026-02-26 04:39:48 +0300

    feat: Add Terraform integration
    Signed-off-by: Jilks Smith <smithjilks@gmail.com>

--- pkg/crypto/hmac.go
+++ pkg/crypto/hmac.go
@@ -3,11 +3,24 @@ package crypto
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
 )
 
 func VerifySignature(key []byte, data []byte, signature string) error {
 	h := hmac.New(sha256.New, key)
+	h.Write(data)
+
+	computed := fmt.Sprintf("%x", h.Sum(nil))
+	if computed != signature {
+		return fmt.Errorf("invalid signature")
+	}
+
+	return nil
+}
+
+func VerifySignatureSHA512(key []byte, data []byte, signature string) error {
+	h := hmac.New(sha512.New, key)
 	h.Write(data)
 
 	computed := fmt.Sprintf("%x", h.Sum(nil))
