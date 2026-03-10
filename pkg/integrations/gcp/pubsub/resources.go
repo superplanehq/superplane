b5493d3693845561a580f7ac20848f1e09b63fae
WashingtonKK <washingtonkigan@gmail.com>
2026-03-10 18:34:27 +0300

    add gcp pub sub integration
    Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

---
+++ pkg/integrations/gcp/pubsub/resources.go
@@ -0,0 +1,50 @@
+package pubsub
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/superplanehq/superplane/pkg/core"
+	gcpcommon "github.com/superplanehq/superplane/pkg/integrations/gcp/common"
+)
+
+const (
+	ResourceTypeTopic        = "gcp.pubsub.topic"
+	ResourceTypeSubscription = "gcp.pubsub.subscription"
+)
+
+func ListTopicResources(ctx context.Context, client *gcpcommon.Client) ([]core.IntegrationResource, error) {
+	projectID := client.ProjectID()
+	topics, err := ListTopics(ctx, client, projectID)
+	if err != nil {
+		return nil, fmt.Errorf("list topics: %w", err)
+	}
+
+	resources := make([]core.IntegrationResource, 0, len(topics))
+	for _, t := range topics {
+		short := TopicShortName(t.Name)
+		resources = append(resources, core.IntegrationResource{
+			ID:   short,
+			Name: short,
+		})
+	}
+	return resources, nil
+}
+
+func ListSubscriptionResources(ctx context.Context, client *gcpcommon.Client) ([]core.IntegrationResource, error) {
+	projectID := client.ProjectID()
+	subs, err := ListSubscriptions(ctx, client, projectID)
+	if err != nil {
+		return nil, fmt.Errorf("list subscriptions: %w", err)
+	}
+
+	resources := make([]core.IntegrationResource, 0, len(subs))
+	for _, s := range subs {
+		short := SubscriptionShortName(s.Name)
+		resources = append(resources, core.IntegrationResource{
+			ID:   short,
+			Name: short,
+		})
+	}
+	return resources, nil
+}
