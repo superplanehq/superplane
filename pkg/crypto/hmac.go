8b102b395752cfb9175ba4adcd30ec4245c5b714
Jilks Smith <smithjilks@gmail.com>
2026-03-11 11:02:58 +0300

    fix: address code review comments
    Signed-off-by: Jilks Smith <smithjilks@gmail.com>

--- pkg/crypto/hmac.go
+++ pkg/crypto/hmac.go
@@ -4,13 +4,20 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"crypto/sha512"
-	"encoding/hex"
 	"fmt"
 	"hash"
 )
 
 func VerifySignature(key []byte, data []byte, signature string) error {
-	h := hmac.New(sha256.New, key)
+	return verifyHMACSignature(sha256.New, key, data, signature)
+}
+
+func VerifySignatureSHA512(key []byte, data []byte, signature string) error {
+	return verifyHMACSignature(sha512.New, key, data, signature)
+}
+
+func verifyHMACSignature(hashFn func() hash.Hash, key []byte, data []byte, signature string) error {
+	h := hmac.New(hashFn, key)
 	h.Write(data)
 
 	computed := fmt.Sprintf("%x", h.Sum(nil))
@@ -25,18 +32,4 @@ func Sign(key []byte, data []byte) string {
 	h := hmac.New(sha256.New, key)
 	h.Write(data)
 	return fmt.Sprintf("%x", h.Sum(nil))
-}
-
-func VerifySignatureSHA512(key []byte, data []byte, signature string) error {
-	h := hmac.New(sha512.New, key)
-	h.Write(data)
-	expected := h.Sum(nil)
-
-	// Decode the provided signature from hex for constant-time comparison.
-	sigBytes, err := hex.DecodeString(signature)
-	if err != nil || !hmac.Equal(expected, sigBytes) {
-		return fmt.Errorf("invalid signature")
-	}
-
-	return nil
 }
