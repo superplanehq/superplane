d9345ad816cea196d608bea73e491bcbf426da4a
Petar Perovic <perovic.petar@gmail.com>
2026-03-23 14:10:28 +0100

    chore(db): revert unintended db/structure.sql drift from versioning UX commit
    Restore casbin_rule dump to match pre-606ff9d40 (ptype NOT NULL, drop idx_casbin_rule block).

--- db/structure.sql
+++ db/structure.sql
@@ -191,7 +191,7 @@ CREATE TABLE public.canvas_memories (
 
 CREATE TABLE public.casbin_rule (
     id integer NOT NULL,
-    ptype character varying(100),
+    ptype character varying(100) NOT NULL,
     v0 character varying(100),
     v1 character varying(100),
     v2 character varying(100),
@@ -1153,13 +1153,6 @@ CREATE INDEX idx_blueprints_organization_id ON public.blueprints USING btree (or
 --
 
 CREATE INDEX idx_canvas_memories_canvas_namespace ON public.canvas_memories USING btree (canvas_id, namespace);
-
-
---
--- Name: idx_casbin_rule; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX idx_casbin_rule ON public.casbin_rule USING btree (ptype, v0, v1, v2, v3, v4, v5);
 
 
 --
