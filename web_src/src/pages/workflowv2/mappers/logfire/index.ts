6d882e504c3d95baaef53d154dd859ca35362fd2
Harsh <harxhist@gmail.com>
2026-03-24 18:19:35 +0530

    chore: CI ESLint fix (#3568)
    Signed-off-by: Harsh <harxhist@gmail.com>

--- web_src/src/pages/workflowv2/mappers/logfire/index.ts
+++ web_src/src/pages/workflowv2/mappers/logfire/index.ts
@@ -1,4 +1,4 @@
-import { ComponentBaseMapper, EventStateRegistry, TriggerRenderer } from "../types";
+import type { ComponentBaseMapper, EventStateRegistry, TriggerRenderer } from "../types";
 import { baseMapper } from "./base";
 import { buildActionStateRegistry } from "../utils";
 import { onAlertReceivedTriggerRenderer } from "./on_alert_received";
