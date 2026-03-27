8fc2652e6d82ecfb8f9baf9b804818c91adafc2d
Lucas Pinheiro <lucas@superplane.com>
2026-03-27 19:34:42 -0300

    keep null default
    Signed-off-by: Lucas Pinheiro <lucas@superplane.com>

--- db/migrations/20260327195840_change-node-state-reason-to-text.up.sql
+++ db/migrations/20260327195840_change-node-state-reason-to-text.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
 ALTER TABLE workflow_nodes ALTER COLUMN state_reason TYPE text;
-ALTER TABLE workflow_nodes ALTER COLUMN state_reason SET DEFAULT '';
+ALTER TABLE workflow_nodes ALTER COLUMN state_reason SET DEFAULT NULL::text;
 
 COMMIT;
