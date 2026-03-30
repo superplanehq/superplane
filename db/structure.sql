d44a6654ff6301579a89b47541791b86bffc9177
Igor Šarčević <igor@operately.com>
2026-03-30 17:33:46 +0200

    chore: Agent Image Building
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- db/structure.sql
+++ db/structure.sql
@@ -5,7 +5,7 @@
 \restrict abcdef123
 
 -- Dumped from database version 17.5 (Debian 17.5-1.pgdg130+1)
--- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg22.04+1)
+-- Dumped by pg_dump version 17.8 (Ubuntu 17.8-1.pgdg22.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -36,21 +36,6 @@ COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UU
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
-
---
--- Name: account_magic_codes; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.account_magic_codes (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
-    email character varying(255) NOT NULL,
-    code_hash character varying(64) NOT NULL,
-    expires_at timestamp without time zone NOT NULL,
-    used_at timestamp without time zone,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    verify_attempts integer DEFAULT 0 NOT NULL
-);
-
 
 --
 -- Name: account_password_auth; Type: TABLE; Schema: public; Owner: -
@@ -207,7 +192,7 @@ CREATE TABLE public.canvas_memories (
 
 CREATE TABLE public.casbin_rule (
     id integer NOT NULL,
-    ptype character varying(100) NOT NULL,
+    ptype character varying(100),
     v0 character varying(100),
     v1 character varying(100),
     v2 character varying(100),
@@ -659,14 +644,6 @@ ALTER TABLE ONLY public.casbin_rule ALTER COLUMN id SET DEFAULT nextval('public.
 
 
 --
--- Name: account_magic_codes account_magic_codes_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.account_magic_codes
-    ADD CONSTRAINT account_magic_codes_pkey PRIMARY KEY (id);
-
-
---
 -- Name: account_password_auth account_password_auth_account_id_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -1075,20 +1052,6 @@ ALTER TABLE ONLY public.workflows
 
 
 --
--- Name: idx_account_magic_codes_email; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_account_magic_codes_email ON public.account_magic_codes USING btree (email);
-
-
---
--- Name: idx_account_magic_codes_email_code_hash; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_account_magic_codes_email_code_hash ON public.account_magic_codes USING btree (email, code_hash);
-
-
---
 -- Name: idx_account_password_auth_account_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -1191,6 +1154,13 @@ CREATE INDEX idx_blueprints_organization_id ON public.blueprints USING btree (or
 --
 
 CREATE INDEX idx_canvas_memories_canvas_namespace ON public.canvas_memories USING btree (canvas_id, namespace);
+
+
+--
+-- Name: idx_casbin_rule; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX idx_casbin_rule ON public.casbin_rule USING btree (ptype, v0, v1, v2, v3, v4, v5);
 
 
 --
@@ -1961,7 +1931,7 @@ ALTER TABLE ONLY public.workflows
 \restrict abcdef123
 
 -- Dumped from database version 17.5 (Debian 17.5-1.pgdg130+1)
--- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg22.04+1)
+-- Dumped by pg_dump version 17.8 (Ubuntu 17.8-1.pgdg22.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -1997,7 +1967,7 @@ COPY public.schema_migrations (version, dirty) FROM stdin;
 \restrict abcdef123
 
 -- Dumped from database version 17.5 (Debian 17.5-1.pgdg130+1)
--- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg22.04+1)
+-- Dumped by pg_dump version 17.8 (Ubuntu 17.8-1.pgdg22.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
