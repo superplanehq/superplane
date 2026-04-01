6166532f0f8c74a372aad281b4d802ad685b8872
Igor Šarčević <igor@operately.com>
2026-04-02 00:52:18 +0200

    Optimized image
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- scripts/docker/install-python.sh
+++ scripts/docker/install-python.sh
@@ -3,17 +3,13 @@
 set -e
 set -o pipefail
 
-echo "Installing Python 3.13 from deadsnakes PPA"
+echo "Installing Python 3.12 from Ubuntu repositories"
 
 apt-get update -y
-apt-get install -y --no-install-recommends software-properties-common
+apt-get install -y --no-install-recommends python3 python3-venv python3-dev
 
-add-apt-repository -y ppa:deadsnakes/ppa
-apt-get update -y
-apt-get install -y --no-install-recommends python3.13 python3.13-venv python3.13-dev
-
-ln -sf /usr/bin/python3.13 /usr/local/bin/python3
-ln -sf /usr/bin/python3.13 /usr/local/bin/python
+ln -sf /usr/bin/python3 /usr/local/bin/python3
+ln -sf /usr/bin/python3 /usr/local/bin/python
 
 apt-get clean
 rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/* /tmp/*
