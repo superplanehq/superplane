9ce79bc612d56621544125ded5736b996b25cf66
Igor Šarčević <igor@operately.com>
2026-04-05 23:15:43 +0200

    Fix version codename
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- scripts/docker/install-postgresql-client.sh
+++ scripts/docker/install-postgresql-client.sh
@@ -8,7 +8,8 @@ echo "Installing PostgreSQL Client 17"
 apt-get update -y
 apt-get install --no-install-recommends -y ca-certificates curl gnupg
 
-echo "deb http://apt.postgresql.org/pub/repos/apt noble-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+. /etc/os-release
+echo "deb http://apt.postgresql.org/pub/repos/apt ${VERSION_CODENAME}-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 curl --retry 5 --retry-delay 1 --retry-max-time 60 --retry-connrefused -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
 
 apt-get update -y
