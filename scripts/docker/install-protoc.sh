87bdd4a5133c1f2f19624fd15577fe40e9f43133
Igor Šarčević <igor@operately.com>
2026-04-05 23:17:42 +0200

    Install unzip
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- scripts/docker/install-protoc.sh
+++ scripts/docker/install-protoc.sh
@@ -7,6 +7,11 @@ echo "Installing Protobuf Compiler (protoc) 3.15.8"
 
 ARCH=$(dpkg --print-architecture)
 
+apt-get update -y
+apt-get install --no-install-recommends -y unzip
+apt-get clean
+rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/*
+
 if [ "$ARCH" = "arm64" ]; then
   curl --retry 5 --retry-delay 1 --retry-max-time 60 --retry-connrefused -sL https://github.com/protocolbuffers/protobuf/releases/download/v3.15.8/protoc-3.15.8-linux-aarch_64.zip -o protoc.zip;
 else
