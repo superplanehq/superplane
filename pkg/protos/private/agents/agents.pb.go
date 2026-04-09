532bdcb38051d14621bb6ff60f46e90c77970413
Pedro F. Leao <pedroforestileao@gmail.com>
2026-04-09 16:26:57 -0300

    update go code
    Signed-off-by: Pedro F. Leao <pedroforestileao@gmail.com>

--- pkg/protos/private/agents/agents.pb.go
+++ pkg/protos/private/agents/agents.pb.go
@@ -746,28 +746,32 @@ func (x *DescribeOrganizationAgentUsageResponse) GetUsage() *ChatUsage {
 	return nil
 }
 
-type AgentTokensUsedMessage struct {
+type AgentRunFinishedMessage struct {
 	state          protoimpl.MessageState `protogen:"open.v1"`
 	OrganizationId string                 `protobuf:"bytes,1,opt,name=organization_id,json=organizationId,proto3" json:"organization_id,omitempty"`
-	Tokens         int64                  `protobuf:"varint,2,opt,name=tokens,proto3" json:"tokens,omitempty"`
+	ChatId         string                 `protobuf:"bytes,2,opt,name=chat_id,json=chatId,proto3" json:"chat_id,omitempty"`
+	Model          string                 `protobuf:"bytes,3,opt,name=model,proto3" json:"model,omitempty"`
+	InputTokens    int64                  `protobuf:"varint,4,opt,name=input_tokens,json=inputTokens,proto3" json:"input_tokens,omitempty"`
+	OutputTokens   int64                  `protobuf:"varint,5,opt,name=output_tokens,json=outputTokens,proto3" json:"output_tokens,omitempty"`
+	TotalTokens    int64                  `protobuf:"varint,6,opt,name=total_tokens,json=totalTokens,proto3" json:"total_tokens,omitempty"`
 	unknownFields  protoimpl.UnknownFields
 	sizeCache      protoimpl.SizeCache
 }
 
-func (x *AgentTokensUsedMessage) Reset() {
-	*x = AgentTokensUsedMessage{}
+func (x *AgentRunFinishedMessage) Reset() {
+	*x = AgentRunFinishedMessage{}
 	mi := &file_private_agents_proto_msgTypes[13]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
 
-func (x *AgentTokensUsedMessage) String() string {
+func (x *AgentRunFinishedMessage) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*AgentTokensUsedMessage) ProtoMessage() {}
+func (*AgentRunFinishedMessage) ProtoMessage() {}
 
-func (x *AgentTokensUsedMessage) ProtoReflect() protoreflect.Message {
+func (x *AgentRunFinishedMessage) ProtoReflect() protoreflect.Message {
 	mi := &file_private_agents_proto_msgTypes[13]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -779,21 +783,49 @@ func (x *AgentTokensUsedMessage) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use AgentTokensUsedMessage.ProtoReflect.Descriptor instead.
-func (*AgentTokensUsedMessage) Descriptor() ([]byte, []int) {
+// Deprecated: Use AgentRunFinishedMessage.ProtoReflect.Descriptor instead.
+func (*AgentRunFinishedMessage) Descriptor() ([]byte, []int) {
 	return file_private_agents_proto_rawDescGZIP(), []int{13}
 }
 
-func (x *AgentTokensUsedMessage) GetOrganizationId() string {
+func (x *AgentRunFinishedMessage) GetOrganizationId() string {
 	if x != nil {
 		return x.OrganizationId
 	}
 	return ""
 }
 
-func (x *AgentTokensUsedMessage) GetTokens() int64 {
+func (x *AgentRunFinishedMessage) GetChatId() string {
 	if x != nil {
-		return x.Tokens
+		return x.ChatId
+	}
+	return ""
+}
+
+func (x *AgentRunFinishedMessage) GetModel() string {
+	if x != nil {
+		return x.Model
+	}
+	return ""
+}
+
+func (x *AgentRunFinishedMessage) GetInputTokens() int64 {
+	if x != nil {
+		return x.InputTokens
+	}
+	return 0
+}
+
+func (x *AgentRunFinishedMessage) GetOutputTokens() int64 {
+	if x != nil {
+		return x.OutputTokens
+	}
+	return 0
+}
+
+func (x *AgentRunFinishedMessage) GetTotalTokens() int64 {
+	if x != nil {
+		return x.TotalTokens
 	}
 	return 0
 }
@@ -851,10 +883,14 @@ const file_private_agents_proto_rawDesc = "" +
 	"%DescribeOrganizationAgentUsageRequest\x12\x15\n" +
 	"\x06org_id\x18\x01 \x01(\tR\x05orgId\"e\n" +
 	"&DescribeOrganizationAgentUsageResponse\x12;\n" +
-	"\x05usage\x18\x01 \x01(\v2%.Superplane.Internal.Agents.ChatUsageR\x05usage\"Y\n" +
-	"\x16AgentTokensUsedMessage\x12'\n" +
-	"\x0forganization_id\x18\x01 \x01(\tR\x0eorganizationId\x12\x16\n" +
-	"\x06tokens\x18\x02 \x01(\x03R\x06tokens2\xb9\x05\n" +
+	"\x05usage\x18\x01 \x01(\v2%.Superplane.Internal.Agents.ChatUsageR\x05usage\"\xdc\x01\n" +
+	"\x17AgentRunFinishedMessage\x12'\n" +
+	"\x0forganization_id\x18\x01 \x01(\tR\x0eorganizationId\x12\x17\n" +
+	"\achat_id\x18\x02 \x01(\tR\x06chatId\x12\x14\n" +
+	"\x05model\x18\x03 \x01(\tR\x05model\x12!\n" +
+	"\finput_tokens\x18\x04 \x01(\x03R\vinputTokens\x12#\n" +
+	"\routput_tokens\x18\x05 \x01(\x03R\foutputTokens\x12!\n" +
+	"\ftotal_tokens\x18\x06 \x01(\x03R\vtotalTokens2\xb9\x05\n" +
 	"\x06Agents\x12z\n" +
 	"\x0fCreateAgentChat\x122.Superplane.Internal.Agents.CreateAgentChatRequest\x1a3.Superplane.Internal.Agents.CreateAgentChatResponse\x12w\n" +
 	"\x0eListAgentChats\x121.Superplane.Internal.Agents.ListAgentChatsRequest\x1a2.Superplane.Internal.Agents.ListAgentChatsResponse\x12\x80\x01\n" +
@@ -889,7 +925,7 @@ var file_private_agents_proto_goTypes = []any{
 	(*ChatUsage)(nil),                              // 10: Superplane.Internal.Agents.ChatUsage
 	(*DescribeOrganizationAgentUsageRequest)(nil),  // 11: Superplane.Internal.Agents.DescribeOrganizationAgentUsageRequest
 	(*DescribeOrganizationAgentUsageResponse)(nil), // 12: Superplane.Internal.Agents.DescribeOrganizationAgentUsageResponse
-	(*AgentTokensUsedMessage)(nil),                 // 13: Superplane.Internal.Agents.AgentTokensUsedMessage
+	(*AgentRunFinishedMessage)(nil),                // 13: Superplane.Internal.Agents.AgentRunFinishedMessage
 	(*timestamp.Timestamp)(nil),                    // 14: google.protobuf.Timestamp
 }
 var file_private_agents_proto_depIdxs = []int32{
