39fd07d2e08d83ba0ad659634f354a667176ff32
WashingtonKK <washingtonkigan@gmail.com>
2026-04-13 13:45:27 +0300

    fix comment
    Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

--- web_src/src/pages/workflowv2/mappers/grafana/get_dashboard.ts
+++ web_src/src/pages/workflowv2/mappers/grafana/get_dashboard.ts
@@ -27,7 +27,7 @@ export const getDashboardMapper: ComponentBaseMapper = {
       eventSections: lastExecution ? buildGrafanaEventSections(context.nodes, lastExecution, componentName) : undefined,
       metadata: buildDashboardMetadata(
         context.node,
-        context.node.configuration as GetDashboardConfiguration | undefined,
+        (context.node.configuration as GetDashboardConfiguration | undefined)?.dashboard,
       ),
       includeEmptyState: !lastExecution,
       eventStateMap: getStateMap(componentName),
