7ab54ddf79d33abd4f794bf25f31931c8fe0cdb1
Marko Đokić <96503127+kameleon1808@users.noreply.github.com>
2026-04-14 20:34:25 +0200

    feat: register HashiCorp Vault integration in server
    Signed-off-by: Marko Đokić <96503127+kameleon1808@users.noreply.github.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -59,6 +59,7 @@ import (
 	_ "github.com/superplanehq/superplane/pkg/integrations/gitlab"
 	_ "github.com/superplanehq/superplane/pkg/integrations/grafana"
 	_ "github.com/superplanehq/superplane/pkg/integrations/harness"
+	_ "github.com/superplanehq/superplane/pkg/integrations/hashicorp_vault"
 	_ "github.com/superplanehq/superplane/pkg/integrations/hetzner"
 	_ "github.com/superplanehq/superplane/pkg/integrations/honeycomb"
 	_ "github.com/superplanehq/superplane/pkg/integrations/incident"
