ce85a55975e5ca75c10da8b802210fcea5ea71e6
Pedro F. Leao <pedroforestileao@gmail.com>
2026-04-16 01:44:03 -0300

    fix: image builds
    Signed-off-by: Pedro F. Leao <pedroforestileao@gmail.com>

--- release/agent-image/build.sh
+++ release/agent-image/build.sh
@@ -20,6 +20,8 @@ IMAGE_REPO="${AGENT_IMAGE_REPO:-ghcr.io/superplanehq/superplane-agent}"
 
 echo "Building SuperPlane Agent image (${IMAGE_REPO})"
 
+make gen.setup.agent
+
 docker buildx build \
   --platform "linux/${ARCH}" \
   --progress=plain \
