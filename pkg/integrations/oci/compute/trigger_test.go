9747e1556df2538b4956a90e9e5e30755e65b215
Gonzalo <gonzalomendezdev@gmail.com>
2026-04-21 16:38:58 -0300

    feat(oci): implement oracle cloud infrastructure integration and compute components
    Signed-off-by: Gonzalo <gonzalomendezdev@gmail.com>

---
+++ pkg/integrations/oci/compute/trigger_test.go
@@ -0,0 +1,82 @@
+package compute
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
+	"github.com/superplanehq/superplane/pkg/core"
+)
+
+type MockIntegrationContext struct {
+	mock.Mock
+	core.IntegrationContext
+}
+
+func (m *MockIntegrationContext) ListSubscriptions() ([]core.IntegrationSubscriptionContext, error) {
+	args := m.Called()
+	return args.Get(0).([]core.IntegrationSubscriptionContext), args.Error(1)
+}
+
+func (m *MockIntegrationContext) Subscribe(data any) (*uuid.UUID, error) {
+	args := m.Called(data)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*uuid.UUID), args.Error(1)
+}
+
+type MockEventContext struct {
+	mock.Mock
+}
+
+func (m *MockEventContext) Emit(payloadType string, payload any) error {
+	args := m.Called(payloadType, payload)
+	return args.Error(0)
+}
+
+func TestOnInstanceCreated_Execute(t *testing.T) {
+	trigger := &OnInstanceCreated{}
+	mockIntegration := new(MockIntegrationContext)
+	mockEvents := new(MockEventContext)
+
+	payload := map[string]any{
+		"eventType": "com.oraclecloud.computeapi.launchinstance.end",
+		"data": map[string]any{
+			"resourceId":   "id1",
+			"resourceName": "test-instance",
+		},
+	}
+
+	mockEvents.On("Emit", "created", mock.Anything).Return(nil)
+
+	msgCtx := core.IntegrationMessageContext{
+		Message:     payload,
+		Integration: mockIntegration,
+		Events:      mockEvents,
+	}
+
+	err := trigger.OnIntegrationMessage(msgCtx)
+	if err != nil {
+		t.Errorf("OnIntegrationMessage failed: %v", err)
+	}
+
+	mockEvents.AssertExpectations(t)
+}
+
+func TestOnInstanceCreated_Setup(t *testing.T) {
+	trigger := &OnInstanceCreated{}
+	mockIntegration := new(MockIntegrationContext)
+
+	mockIntegration.On("Subscribe", "oci.onInstanceCreated").Return(nil, nil)
+
+	setupCtx := core.TriggerContext{
+		Integration: mockIntegration,
+	}
+
+	err := trigger.Setup(setupCtx)
+	if err != nil {
+		t.Errorf("Setup failed: %v", err)
+	}
+	mockIntegration.AssertExpectations(t)
+}
