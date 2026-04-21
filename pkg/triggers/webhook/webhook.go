04a395d5e9c09b0bd387b1bb199e1a26515d73a4
Pedro F. Leao <pedroforestileao@gmail.com>
2026-04-21 00:07:02 -0300

    refactor: simplify webhook run title fallback
    Signed-off-by: Pedro F. Leao <pedroforestileao@gmail.com>

--- pkg/triggers/webhook/webhook.go
+++ pkg/triggers/webhook/webhook.go
@@ -96,7 +96,7 @@ func (w *Webhook) Color() string {
 }
 
 func (w *Webhook) DefaultRunTitle() string {
-	return `{{ firstNonEmpty(root().data.body.run_name, "Webhook " + root().id + " at " + date(root().timestamp).Format("1/2/2006, 3:04:05 PM")) }}`
+	return `{{ firstNonEmpty(root().data.body.run_name, root().id) }}`
 }
 
 func (w *Webhook) Configuration() []configuration.Field {
