9d53fc6bbaf33fd7952698e3260cd678a06089f5
Milos Jovanovic <milosjovanovic519@gmail.com>
2026-05-01 01:00:37 +0200

    feat: enforce OAuth allowed_providers for invites and expose in org spec
    Signed-off-by: Milos Jovanovic <milosjovanovic519@gmail.com>

---
+++ pkg/grpc/actions/organizations/organization_proto.go
@@ -0,0 +1,30 @@
+package organizations
+
+import (
+	"github.com/superplanehq/superplane/pkg/models"
+	pb "github.com/superplanehq/superplane/pkg/protos/organizations"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func organizationToProto(organization *models.Organization) *pb.Organization {
+	providers := []string(organization.AllowedProviders)
+	if providers == nil {
+		providers = []string{}
+	}
+	return &pb.Organization{
+		Metadata: &pb.Organization_Metadata{
+			Id:          organization.ID.String(),
+			Name:        organization.Name,
+			Description: organization.Description,
+			CreatedAt:   timestamppb.New(*organization.CreatedAt),
+			UpdatedAt:   timestamppb.New(*organization.UpdatedAt),
+		},
+		Spec: &pb.Organization_Spec{
+			ChangeManagementEnabled:     &organization.ChangeManagementEnabled,
+			EnabledExperimentalFeatures: []string(organization.EnabledExperimentalFeatures),
+			AllowedOauthProviders: &pb.Organization_AllowedOAuthProviders{
+				Providers: providers,
+			},
+		},
+	}
+}
