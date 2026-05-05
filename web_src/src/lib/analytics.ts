3a8ad7a7db140aa961782846fd357356dd7c93a9
Dragica Draskic <dragica.draskic@gmail.com>
2026-05-05 13:20:45 +0200

    fix: remove unnecessary event
    Signed-off-by: Dragica Draskic <dragica.draskic@gmail.com>

--- web_src/src/lib/analytics.ts
+++ web_src/src/lib/analytics.ts
@@ -51,9 +51,9 @@ export const analytics = {
     posthog.capture("request_integration_clicked", { organization_id: organizationId });
   },
 
-  organizationJoined: (organizationId: string) => {
-    posthog.capture("organization joined", { organization_id: organizationId });
-  },
+  // organizationJoined: (organizationId: string) => {
+  //   posthog.capture("organization joined", { organization_id: organizationId });
+  // },
 
   yamlImport: () => {
     posthog.capture("canvas:yaml_import", {});
