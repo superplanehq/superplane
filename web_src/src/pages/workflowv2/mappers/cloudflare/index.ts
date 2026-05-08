3064e7d13348d4cbbf2ae87b1fa52345310fd4d2
Washington Kigani Kamadi <washingtonkigan@gmail.com>
2026-05-08 15:36:01 +0300

    feat: add cloudflare create, update and delete origin rule components (#4669)
    Closes: https://github.com/superplanehq/superplane/issues/4642
    Related to: https://github.com/superplanehq/superplane/issues/4454
    
    This expands the cloudflare integration by adding the following origin
    rule components.
    
    - `cloudflare.createOriginRule`
    - `cloudflare.updateOriginRule`
    - `cloudflare.deleteOriginRule`
    
    ### Demo
    
    ---------
    
    Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

--- web_src/src/pages/workflowv2/mappers/cloudflare/index.ts
+++ web_src/src/pages/workflowv2/mappers/cloudflare/index.ts
@@ -1,19 +1,26 @@
 import type { ComponentBaseMapper, EventStateRegistry, TriggerRenderer } from "../types";
 import { baseMapper } from "./base";
 import { buildActionStateRegistry } from "../utils";
+import { originRuleMapper } from "./origin_rule";
 
 export const componentMappers: Record<string, ComponentBaseMapper> = {
   createDnsRecord: baseMapper,
+  createOriginRule: originRuleMapper,
   updateDNSRecord: baseMapper,
   deleteDnsRecord: baseMapper,
   updateRedirectRule: baseMapper,
+  updateOriginRule: originRuleMapper,
+  deleteOriginRule: originRuleMapper,
 };
 
 export const triggerRenderers: Record<string, TriggerRenderer> = {};
 
 export const eventStateRegistry: Record<string, EventStateRegistry> = {
   createDnsRecord: buildActionStateRegistry("completed"),
+  createOriginRule: buildActionStateRegistry("created"),
   updateDNSRecord: buildActionStateRegistry("completed"),
   deleteDnsRecord: buildActionStateRegistry("completed"),
   updateRedirectRule: buildActionStateRegistry("completed"),
+  updateOriginRule: buildActionStateRegistry("updated"),
+  deleteOriginRule: buildActionStateRegistry("deleted"),
 };
