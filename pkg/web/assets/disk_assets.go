baaf4a2d1aded21ea15c2f1adb2ef8032199dbe6
Akerele Abraham <abrahamakerele38@gmail.com>
2026-05-14 22:10:31 +0100

    Prevent directory traversal in diskFS.Open

--- pkg/web/assets/disk_assets.go
+++ pkg/web/assets/disk_assets.go
@@ -6,6 +6,9 @@ package assets
 import (
 	"io/fs"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -16,7 +19,18 @@ type diskFS struct {
 
 func (d diskFS) Open(name string) (fs.File, error) {
 	log.Infof("Opening asset %s/%s", d.root, name)
-	return os.Open(d.root + "/" + name)
+
+	clean := path.Clean("/" + name)
+	full := filepath.Join(d.root, clean)
+
+	rel, err := filepath.Rel(d.root, full)
+	if err != nil {
+		return nil, err
+	}
+	if strings.HasPrefix(rel, "..") {
+		return nil, fs.ErrPermission
+	}
+	return os.Open(full)
 }
 
 var EmbeddedAssets fs.FS = diskFS{root: "pkg/web/assets/dist"}
