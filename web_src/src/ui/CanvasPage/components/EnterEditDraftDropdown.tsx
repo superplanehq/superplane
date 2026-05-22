34f43426619fa52491870677dd0da608512829d7
Pedro F. Leao <pedroforestileao@gmail.com>
2026-05-22 13:16:49 -0300

    fix: align continue editing casing
    Signed-off-by: Pedro F. Leao <pedroforestileao@gmail.com>

--- web_src/src/ui/CanvasPage/components/EnterEditDraftDropdown.tsx
+++ web_src/src/ui/CanvasPage/components/EnterEditDraftDropdown.tsx
@@ -38,7 +38,7 @@ export function EnterEditDraftDropdown({
         <div className="py-1">
           <DropdownMenuItem onClick={onContinueEditing} className="gap-2 px-3 py-2 text-slate-700 cursor-pointer">
             <Pencil className="h-4 w-4" />
-            <span>Continue editing</span>
+            <span>Continue Editing</span>
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={onDiscardAndStartEdit}
