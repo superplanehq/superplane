5fcf2d708b6c8c5dc686095cbd5769e4308e303d
Lucas Pinheiro <lucas@superplane.com>
2026-06-03 15:47:38 -0300

    chore: update app URL in CLI outputs
    Signed-off-by: Lucas Pinheiro <lucas@superplane.com>

--- pkg/cli/commands/apps/canvas/get.go
+++ pkg/cli/commands/apps/canvas/get.go
@@ -73,8 +73,8 @@ func (c *getCommand) Execute(ctx core.CommandContext) error {
 	return ctx.Renderer.RenderText(func(stdout io.Writer) error {
 		_, _ = fmt.Fprintf(stdout, "ID: %s\n", resource.Metadata.GetId())
 		_, _ = fmt.Fprintf(stdout, "Name: %s\n", resource.Metadata.GetName())
-		if url := common.BuildCanvasURL(ctx, canvas.Metadata.GetOrganizationId(), canvas.Metadata.GetId()); url != "" {
-			_, _ = fmt.Fprintf(stdout, "Canvas URL: %s\n", url)
+		if url := common.BuildAppURL(ctx, canvas.Metadata.GetOrganizationId(), canvas.Metadata.GetId()); url != "" {
+			_, _ = fmt.Fprintf(stdout, "App URL: %s\n", url)
 		}
 		_, _ = fmt.Fprintf(stdout, "Nodes: %d\n", len(resource.Spec.GetNodes()))
 		_, err := fmt.Fprintf(stdout, "Edges: %d\n", len(resource.Spec.GetEdges()))
