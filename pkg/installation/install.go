9435df67856c29bf55a4b5ea97ab5c74496520d2
Igor Šarčević <igisar@gmail.com>
2026-06-15 11:53:42 +0200

    chore: Traces inside of the authorization layer (#5472)
    related to https://github.com/superplanehq/superplane/issues/5470
    
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- pkg/installation/install.go
+++ pkg/installation/install.go
@@ -67,8 +67,7 @@ func (s *Service) Install(ctx context.Context, req InstallRequest) (*InstallResu
 		return nil, err
 	}
 
-	allowed, err := s.AuthService.CheckOrganizationPermission(
-		user.ID.String(),
+	allowed, err := s.AuthService.CheckOrganizationPermission(context.Background(), user.ID.String(),
 		req.OrganizationID.String(),
 		"canvases",
 		"create",
