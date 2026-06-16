e56cb2187ab8e5256b4ba1f77626d25c8101acd9
Pedro F. Leao <pedroforestileao@gmail.com>
2026-06-16 17:22:36 -0300

    feat: add agent repository file tools
    Signed-off-by: Pedro F. Leao <pedroforestileao@gmail.com>

--- web_src/src/pages/app/files/useCatalog.ts
+++ web_src/src/pages/app/files/useCatalog.ts
@@ -45,27 +45,29 @@ export function useRepositoryPathLists(
   generatedPaths: string[],
   repositoryPaths: string[],
   pendingChanges: PendingFileChange[],
+  stagedRepositoryPaths: string[] = [],
 ) {
   const repositoryAndPendingPaths = useMemo(() => {
     return Array.from(
       new Set([
         ...repositoryPaths,
+        ...stagedRepositoryPaths,
         ...pendingChanges.filter((change) => change.type === "added").map((change) => change.path),
       ]),
     ).sort();
-  }, [pendingChanges, repositoryPaths]);
+  }, [pendingChanges, repositoryPaths, stagedRepositoryPaths]);
   const allPaths = useMemo(
     () => Array.from(new Set([...generatedPaths, ...repositoryAndPendingPaths])).sort(),
     [generatedPaths, repositoryAndPendingPaths],
   );
   const visiblePaths = useMemo(() => {
     return Array.from(
-      new Set([...generatedPaths, ...buildRenderableTreePaths(repositoryPaths, pendingChanges)]),
+      new Set([...generatedPaths, ...buildRenderableTreePaths(repositoryAndPendingPaths, pendingChanges)]),
     ).sort();
-  }, [generatedPaths, pendingChanges, repositoryPaths]);
+  }, [generatedPaths, pendingChanges, repositoryAndPendingPaths]);
   const finalRepositoryPaths = useMemo(
-    () => buildFinalRepositoryPaths(repositoryPaths, pendingChanges),
-    [pendingChanges, repositoryPaths],
+    () => buildFinalRepositoryPaths(repositoryAndPendingPaths, pendingChanges),
+    [pendingChanges, repositoryAndPendingPaths],
   );
   const commitPathError = useMemo(
     () => getPathValidationError([...generatedPaths, ...finalRepositoryPaths]),
