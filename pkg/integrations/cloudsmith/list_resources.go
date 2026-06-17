26c2c117c1b81bd305b8642d7c36a00bcabbfc19
Felix Gateru <felix.gateru@gmail.com>
2026-06-17 14:26:25 +0300

    feat(cloudsmith): include version in package resource name
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

--- pkg/integrations/cloudsmith/list_resources.go
+++ pkg/integrations/cloudsmith/list_resources.go
@@ -69,9 +69,9 @@ func listPackages(ctx core.ListResourcesContext) ([]core.IntegrationResource, er
 
 	resources := make([]core.IntegrationResource, 0, len(packages))
 	for _, pkg := range packages {
-		name := pkg.Slug
-		if name == "" {
-			name = pkg.SlugPerm
+		name := pkg.Name
+		if pkg.Version != "" {
+			name = fmt.Sprintf("%s %s", name, pkg.Version)
 		}
 		resources = append(resources, core.IntegrationResource{
 			Type: "package",
