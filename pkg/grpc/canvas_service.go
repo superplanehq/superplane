b711d6463fdaa9c351432a2f28345fa7f50ba6dc
Lucas Pinheiro <lucas@superplane.com>
2026-06-17 15:04:56 -0300

    chore: remove unused /events API endpoint
    Signed-off-by: Lucas Pinheiro <lucas@superplane.com>

--- pkg/grpc/canvas_service.go
+++ pkg/grpc/canvas_service.go
@@ -274,15 +274,6 @@ func (s *CanvasService) InvokeNodeTriggerHook(ctx context.Context, req *pb.Invok
 	)
 }
 
-func (s *CanvasService) ListCanvasEvents(ctx context.Context, req *pb.ListCanvasEventsRequest) (*pb.ListCanvasEventsResponse, error) {
-	canvasID, err := uuid.Parse(req.CanvasId)
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid workflow_id")
-	}
-
-	return canvases.ListCanvasEvents(ctx, s.registry, canvasID, req.Limit, req.Before)
-}
-
 func (s *CanvasService) ListRuns(ctx context.Context, req *pb.ListRunsRequest) (*pb.ListRunsResponse, error) {
 	canvasID, err := uuid.Parse(req.CanvasId)
 	if err != nil {
