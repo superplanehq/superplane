70590f09fec9ae514bd931be87f2c144245c2435
Felix Gateru <felix.gateru@gmail.com>
2026-07-02 15:51:56 +0300

    feat: add Cloudsmith listPackages and promotePackages components (#5658)
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>
    Co-authored-by: Pedro Leão <60622592+forestileao@users.noreply.github.com>

--- web_src/src/pages/app/mappers/cloudsmith/index.ts
+++ web_src/src/pages/app/mappers/cloudsmith/index.ts
@@ -2,6 +2,8 @@ import type { ComponentBaseMapper, EventStateRegistry, TriggerRenderer } from ".
 import { buildActionStateRegistry } from "../utils";
 import { getRepositoryMapper } from "./get_repository";
 import { getPackageMapper } from "./get_package";
+import { listPackagesMapper } from "./list_packages";
+import { promotePackageMapper, promotePackageEventStateRegistry } from "./promote_package";
 import { onSecurityScanCompletedTriggerRenderer } from "./on_security_scan_completed";
 import { onPackageCreatedTriggerRenderer } from "./on_package_created";
 import { resyncPackageMapper } from "./resync_package";
@@ -14,6 +16,8 @@ export const componentMappers: Record<string, ComponentBaseMapper> = {
   resyncPackage: resyncPackageMapper,
   tagPackage: tagPackageMapper,
   deletePackage: deletePackageMapper,
+  listPackages: listPackagesMapper,
+  promotePackage: promotePackageMapper,
 };
 
 export const triggerRenderers: Record<string, TriggerRenderer> = {
@@ -24,6 +28,8 @@ export const triggerRenderers: Record<string, TriggerRenderer> = {
 export const eventStateRegistry: Record<string, EventStateRegistry> = {
   getRepository: buildActionStateRegistry("fetched"),
   getPackage: buildActionStateRegistry("fetched"),
+  listPackages: buildActionStateRegistry("completed"),
+  promotePackage: promotePackageEventStateRegistry,
   onSecurityScanCompleted: buildActionStateRegistry("triggered"),
   onPackageCreated: buildActionStateRegistry("triggered"),
   resyncPackage: buildActionStateRegistry("resynced"),
