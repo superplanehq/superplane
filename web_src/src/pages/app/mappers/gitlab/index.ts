2de79613618782ae32d87652eb4baee944093092
WashingtonKK <washingtonkigan@gmail.com>
2026-07-13 17:25:30 +0300

    feat: add GitLab Accept Merge Request, Approve Merge Request, and Get Issue components
    Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

--- web_src/src/pages/app/mappers/gitlab/index.ts
+++ web_src/src/pages/app/mappers/gitlab/index.ts
@@ -1,8 +1,11 @@
 import type { ComponentBaseMapper, EventStateRegistry, TriggerRenderer } from "../types";
 import { buildActionStateRegistry } from "../utils";
+import { acceptMergeRequestMapper } from "./accept_merge_request";
 import { addReactionMapper } from "./add_reaction";
+import { approveMergeRequestMapper } from "./approve_merge_request";
 import { createIssueMapper } from "./create_issue";
 import { createMergeCommentMapper } from "./create_merge_comment";
+import { getIssueMapper } from "./get_issue";
 import { onIssueTriggerRenderer } from "./on_issue";
 import { onMergeCommentTriggerRenderer } from "./on_merge_comment";
 import { onMergeRequestTriggerRenderer } from "./on_merge_request";
@@ -16,22 +19,28 @@ import { pipelineLookupMapper, testReportSummaryMapper } from "./pipeline_action
 
 export const eventStateRegistry: Record<string, EventStateRegistry> = {
   createIssue: buildActionStateRegistry("created"),
+  getIssue: buildActionStateRegistry("retrieved"),
   runPipeline: RUN_PIPELINE_STATE_REGISTRY,
   getPipeline: buildActionStateRegistry("retrieved"),
   getLatestPipeline: buildActionStateRegistry("retrieved"),
   getTestReportSummary: buildActionStateRegistry("retrieved"),
   createMergeComment: buildActionStateRegistry("created"),
   addReaction: buildActionStateRegistry("added"),
+  acceptMergeRequest: buildActionStateRegistry("merged"),
+  approveMergeRequest: buildActionStateRegistry("approved"),
 };
 
 export const componentMappers: Record<string, ComponentBaseMapper> = {
   createIssue: createIssueMapper,
+  getIssue: getIssueMapper,
   runPipeline: runPipelineMapper,
   getPipeline: pipelineLookupMapper,
   getLatestPipeline: pipelineLookupMapper,
   getTestReportSummary: testReportSummaryMapper,
   createMergeComment: createMergeCommentMapper,
   addReaction: addReactionMapper,
+  acceptMergeRequest: acceptMergeRequestMapper,
+  approveMergeRequest: approveMergeRequestMapper,
 };
 
 export const triggerRenderers: Record<string, TriggerRenderer> = {
