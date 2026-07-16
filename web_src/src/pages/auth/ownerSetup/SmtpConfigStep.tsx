1faba9e296126d63bd23ff79fa57f8915d99df61
Lucas Pinheiro <lucas@superplane.com>
2026-07-16 15:56:24 -0300

    fix: apply dark theme on owner setup pages
    Signed-off-by: Lucas Pinheiro <lucas@superplane.com>

--- web_src/src/pages/auth/ownerSetup/SmtpConfigStep.tsx
+++ web_src/src/pages/auth/ownerSetup/SmtpConfigStep.tsx
@@ -68,7 +68,7 @@ const SmtpConfigFields: React.FC<SmtpConfigFieldsProps> = ({
   <>
     <div>
       <Label className="mb-2 block text-left">
-        SMTP Host <span className="text-gray-800">*</span>
+        SMTP Host <span className="text-gray-800 dark:text-gray-300">*</span>
       </Label>
       <InputGroup>
         <Input
@@ -84,7 +84,7 @@ const SmtpConfigFields: React.FC<SmtpConfigFieldsProps> = ({
 
     <div>
       <Label className="mb-2 block text-left">
-        SMTP Port <span className="text-gray-800">*</span>
+        SMTP Port <span className="text-gray-800 dark:text-gray-300">*</span>
       </Label>
       <InputGroup>
         <Input
@@ -140,7 +140,7 @@ const SmtpConfigFields: React.FC<SmtpConfigFieldsProps> = ({
 
     <div>
       <Label className="mb-2 block text-left">
-        From Email <span className="text-gray-800">*</span>
+        From Email <span className="text-gray-800 dark:text-gray-300">*</span>
       </Label>
       <InputGroup>
         <Input
