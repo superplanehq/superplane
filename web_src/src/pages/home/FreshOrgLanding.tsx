6ff7415d2fd939d27e8f3ab71426d2559cf360de
Pedro F. Leao <pedroforestileao@gmail.com>
2026-07-24 09:56:09 -0300

    fix label
    Signed-off-by: Pedro F. Leao <pedroforestileao@gmail.com>

--- web_src/src/pages/home/FreshOrgLanding.tsx
+++ web_src/src/pages/home/FreshOrgLanding.tsx
@@ -185,7 +185,7 @@ function StarterAppsCatalog({
   return (
     <div className="mt-10 flex flex-col gap-3">
       <p className="text-xs font-normal text-gray-600 dark:text-gray-400">
-        Automation starters (not Software Factory setup)
+        Automation starters
       </p>
       {apps.map((app) => (
         <StarterAppListItem key={app.repo} app={app} busy={busy} onSetup={() => onSetup(app)} />
