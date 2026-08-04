bba1f5d69ec07995efc9d1bb747fe5e33ed8e670
Washington Kigani Kamadi <washingtonkigan@gmail.com>
2026-08-04 16:48:52 +0300

    feat: add content filter to Linear on issue comment trigger (#6535)
    Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>
    Co-authored-by: Pedro Leão <60622592+forestileao@users.noreply.github.com>

--- web_src/src/pages/app/mappers/linear/types.ts
+++ web_src/src/pages/app/mappers/linear/types.ts
@@ -115,6 +115,7 @@ export interface OnIssueLabelConfiguration {
 export interface OnIssueCommentConfiguration {
   team?: string;
   actions?: string[];
+  contentFilter?: string;
 }
 
 export interface GetIssueConfiguration {
