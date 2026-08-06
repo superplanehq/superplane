3c776f9219fa3413044e9672b4c77f2a676b52a1
Washington Kigani Kamadi <washingtonkigan@gmail.com>
2026-08-06 13:18:11 +0300

    feat: add Linear attachment components (#6556)
    Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>
    Co-authored-by: Pedro Leão <60622592+forestileao@users.noreply.github.com>

--- pkg/grpc/actions/run_title_defaults.go
+++ pkg/grpc/actions/run_title_defaults.go
@@ -89,6 +89,7 @@ var defaultRunTitleExpressions = map[string]string{
 	"linear.onIssue":                      "{{ root().data.data.identifier }} - {{ root().data.data.title }}",
 	"linear.onIssueComment":               "{{ root().data.data.issue.identifier }} - {{ root().data.data.issue.title }}",
 	"linear.onIssueLabel":                 "{{ root().data.data.identifier }} - {{ root().data.data.title }}",
+	"linear.onIssueAttachment":            "{{ root().data.data.issue != nil ? root().data.data.issue.identifier + \" - \" + root().data.data.title : root().data.data.title }}",
 	"logfire.onAlertReceived":             "{{ root().data.alertName }}",
 	"newrelic.onIssue":                    "{{ root().data.title }} {{ root().data.state }}",
 	"oci.onComputeInstanceCreated":        "{{ root().data.data.resourceName }}",
