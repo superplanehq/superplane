186f319b5db748cd0a6d77884f2aa58a9dd9434a
Felix Gateru <felix.gateru@gmail.com>
2026-08-13 15:36:36 +0300

    refactor(aws): fold logs_client_test.go into partition_test.go
    The alarm/metric Client's endpoint-partition test already lives in
    partition_test.go; move LogsClient's China-region regression test
    next to it instead of keeping a separate file.
    
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

--- pkg/integrations/aws/cloudwatch/partition_test.go
+++ pkg/integrations/aws/cloudwatch/partition_test.go
@@ -1,9 +1,15 @@
 package cloudwatch
 
 import (
+	"io"
+	"net/http"
+	"strings"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/superplanehq/superplane/test/support/contexts"
 )
 
 func Test__PartitionForRegion(t *testing.T) {
@@ -37,6 +43,32 @@ func Test__ClientEndpoint(t *testing.T) {
 	t.Run("china regions use the .cn suffix", func(t *testing.T) {
 		client := NewClient(nil, nil, "cn-north-1")
 		assert.Equal(t, "https://monitoring.cn-north-1.amazonaws.com.cn/", client.endpoint)
+	})
+}
+
+func Test__LogsClientEndpoint(t *testing.T) {
+	t.Run("standard regions", func(t *testing.T) {
+		httpContext := &contexts.HTTPContext{
+			Responses: []*http.Response{{StatusCode: http.StatusOK, Body: io.NopCloser(strings.NewReader("{}"))}},
+		}
+
+		client := NewLogsClient(httpContext, &aws.Credentials{AccessKeyID: "key", SecretAccessKey: "secret"}, "us-east-1")
+		require.NoError(t, client.CreateLogStream("my-log-group", "my-stream"))
+
+		require.Len(t, httpContext.Requests, 1)
+		assert.Equal(t, "https://logs.us-east-1.amazonaws.com/", httpContext.Requests[0].URL.String())
+	})
+
+	t.Run("china regions use the .cn suffix", func(t *testing.T) {
+		httpContext := &contexts.HTTPContext{
+			Responses: []*http.Response{{StatusCode: http.StatusOK, Body: io.NopCloser(strings.NewReader("{}"))}},
+		}
+
+		client := NewLogsClient(httpContext, &aws.Credentials{AccessKeyID: "key", SecretAccessKey: "secret"}, "cn-north-1")
+		require.NoError(t, client.CreateLogStream("my-log-group", "my-stream"))
+
+		require.Len(t, httpContext.Requests, 1)
+		assert.Equal(t, "https://logs.cn-north-1.amazonaws.com.cn/", httpContext.Requests[0].URL.String())
 	})
 }
 
