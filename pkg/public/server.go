77a8750bc8c4c697d210ba1a307f696b8245e318
Rohit Lahori <rohitlahori.02@gmail.com>
2026-08-29 18:01:32 -0500

    fix: Record canvas event publish failures
    Signed-off-by: Rohit Lahori <rohitlahori.02@gmail.com>

--- pkg/public/server.go
+++ pkg/public/server.go
@@ -834,7 +834,9 @@ func (s *Server) dispatchIntegrationRequest(w http.ResponseWriter, r *http.Reque
 	}
 
 	for _, event := range newEvents {
-		messages.PublishCanvasEventCreatedMessage(&event)
+		if err := messages.PublishCanvasEventCreatedMessage(&event); err != nil {
+			log.Errorf("failed to publish canvas event created RabbitMQ message for event %s in canvas %s: %v", event.ID, event.WorkflowID, err)
+		}
 	}
 }
 
@@ -1289,7 +1291,9 @@ func (s *Server) HandleWebhook(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, event := range newEvents {
-		messages.PublishCanvasEventCreatedMessage(&event)
+		if err := messages.PublishCanvasEventCreatedMessage(&event); err != nil {
+			log.Errorf("failed to publish canvas event created RabbitMQ message for event %s in canvas %s: %v", event.ID, event.WorkflowID, err)
+		}
 	}
 
 	for executionID, workflowID := range touchedExecutions {
