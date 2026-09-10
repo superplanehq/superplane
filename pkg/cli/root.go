c559631b5056ca09528143f2cdbeeb1a54d8f638
SuperPlane Agent <superplaneagent@superplane.com>
2026-09-10 21:36:32 +0000

    fix(cli): Keep executions and queue commands removed
    Main already removed the executions and queue CLI commands.
    The earlier merge from a shallow clone restored those commands.
    This change drops them again so this branch matches main.
    
    Signed-off-by: SuperPlane Agent <superplaneagent@superplane.com>

--- pkg/cli/root.go
+++ pkg/cli/root.go
@@ -11,14 +11,12 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	apps "github.com/superplanehq/superplane/pkg/cli/commands/apps"
-	executions "github.com/superplanehq/superplane/pkg/cli/commands/executions"
 	groups "github.com/superplanehq/superplane/pkg/cli/commands/groups"
 	index "github.com/superplanehq/superplane/pkg/cli/commands/index"
 	integrations "github.com/superplanehq/superplane/pkg/cli/commands/integrations"
 	members "github.com/superplanehq/superplane/pkg/cli/commands/members"
 	oidc "github.com/superplanehq/superplane/pkg/cli/commands/oidc"
 	organizations "github.com/superplanehq/superplane/pkg/cli/commands/organizations"
-	queue "github.com/superplanehq/superplane/pkg/cli/commands/queue"
 	roles "github.com/superplanehq/superplane/pkg/cli/commands/roles"
 	runs "github.com/superplanehq/superplane/pkg/cli/commands/runs"
 	secrets "github.com/superplanehq/superplane/pkg/cli/commands/secrets"
@@ -61,7 +59,6 @@ func init() {
 
 	options := defaultBindOptions()
 	RootCmd.AddCommand(apps.NewCommand(options))
-	RootCmd.AddCommand(executions.NewCommand(options))
 	RootCmd.AddCommand(runs.NewCommand(options))
 	RootCmd.AddCommand(groups.NewCommand(options))
 	RootCmd.AddCommand(index.NewCommand(options))
@@ -69,7 +66,6 @@ func init() {
 	RootCmd.AddCommand(members.NewCommand(options))
 	RootCmd.AddCommand(oidc.NewCommand(options))
 	RootCmd.AddCommand(organizations.NewCommand(options))
-	RootCmd.AddCommand(queue.NewCommand(options))
 	RootCmd.AddCommand(roles.NewCommand(options))
 	RootCmd.AddCommand(secrets.NewCommand(options))
 	RootCmd.AddCommand(tasks.NewCommand(options))
