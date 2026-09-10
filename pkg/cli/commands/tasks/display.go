2cce4640a4b7dfe7c7760eff241b1e410dcf3169
SuperPlane Agent <superplaneagent@superplane.com>
2026-09-10 21:46:12 +0000

    fix(cli): Format display.go struct alignment
    The eventArtifactRef struct had inconsistent field tag alignment after
    changing 'interface{}' to 'any'. Apply gofmt to fix the spacing.
    
    Signed-off-by: SuperPlane Agent <superplaneagent@superplane.com>

--- pkg/cli/commands/tasks/display.go
+++ pkg/cli/commands/tasks/display.go
@@ -189,8 +189,8 @@ type eventRunRef struct {
 }
 
 type eventArtifactRef struct {
-	ID   string                 `json:"id"`
-	Type string                 `json:"type"`
+	ID   string         `json:"id"`
+	Type string         `json:"type"`
 	Data map[string]any `json:"data,omitempty"`
 }
 
